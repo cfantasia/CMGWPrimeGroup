88842f62791970fa37434ca69ab28392d19e152a
cleonido <>
2008-11-05 15:13:12 +0000

    first version of documentation, history version and installation script

---
+++ doc/README.txt
@@ -0,0 +1,20 @@
+(1) Copy and run the installation script: 
+UserCode/CMGWPrimeGroup/macros/setup_Wprime.sh
+
+It will:
+
+o Setup a new working area (Default name: WPrime_work, release:
+CMSSW_2_1_&)
+
+o Check out the UserCode/CMGWPrimeGroup package (default version:
+V00-00-10)
+
+o Compile the code
+
+o Copy example config file to the top directory.
+
+
+(2) You can now modify the parameters in configuration file, change the
+location of the input file and run.
+
+
---
+++ macros/setup_Wprime.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+if [ -f /etc/bashrc ]; then
+      . /etc/bashrc
+else
+echo -e " No /etc/bashrc file! So confused..."
+echo -e " Reflect, repent, and try with a better script next time"
+echo -e " Exiting...."
+exit
+fi
+
+# Definitions
+RELEASE_VERSION=CMSSW_2_1_7
+WORKING_AREA=WPrime_work
+# end definitions
+
+export RELEASE_VERSION WORKING_AREA
+
+echo -e  "******************************************"
+echo -en " Setting up new working area: "
+echo -e $WORKING_AREA
+echo -e  "******************************************"
+
+#setup new working area
+mkdir $WORKING_AREA
+cd $WORKING_AREA
+scramv1 p CMSSW $RELEASE_VERSION
+cd $RELEASE_VERSION/src
+
+echo -e  "\n**************************"
+echo -e  " Checking out the code..."
+echo -e  "**************************"
+cvs -Q co V00-00-10 UserCode/CMGWPrimeGroup
+
+echo -e "\n************************"
+echo -e " Done. Now will compile"
+echo -e "************************"
+sleep 2
+source /afs/cern.ch/cms/sw/cmsset_default.sh
+# Not sure how to make script aware of cmsenv alias...
+#cmsenv
+scramv1 runtime -sh
+scramv1 b # (lots of output here, but nothing to worry about)
+#cd ../../../
+
+echo -e "\n*************************************************"
+echo -e " Done compiling UserCode/CMGWPrimeGroup"
+echo -e " Now making symbolic link to example config file"
+echo -e "*************************************************"
+echo -e " \n"
+ln -s UserCode/CMGWPrimeGroup/test/myanalysis_TeVMuon.cfg .
