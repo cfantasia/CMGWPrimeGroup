6c5f37e987b8941a95c8bef7bf092b2344e4ab20
cleonido <>
2009-03-16 16:10:28 +0000

    helper struct for vector of input files

---
+++ root_macros/input_file.h
@@ -0,0 +1,29 @@
+#ifndef _wprime_input_file_h
+#define _wprime_input_file_h
+
+#include <vector>
+#include <string>
+
+class TTree;
+
+namespace wprime{
+
+  struct InputFile
+  {
+    float x_sect; // cross-section in pb
+    int Nprod_evt; // # of events produced
+    float weight; // cross-section * integrated luminosity / (# of events produced)
+    std::string pathname;
+    std::string description; // sample description
+    TTree * tree; // pointer to ROOT file
+    //
+    InputFile(float cross_sect, int nprod_evt)
+    {
+      x_sect = cross_sect; Nprod_evt = nprod_evt; weight = 0; tree = 0;
+    }
+  };
+}
+
+
+
+#endif // _wprime_input_file_h
