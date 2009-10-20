8966210569df2d490f6e9491c19a29ba8e3a0441
cleonido <>
2009-10-20 15:19:21 +0000

    updates

--- root_macros/Make.C
+++ root_macros/Make.C
@@ -12,6 +12,9 @@
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/loadInputFiles.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/loadCrossSections.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/loadCuts.C+");
+  gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/util.C+");
+
+  gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/GetChargePtDistribution.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/GetMuonPtDistribution.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/GetMuonPtDistribution_JetIso.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/GetDistributionGeneric.C+");
@@ -28,34 +31,41 @@
 
   gROOT->ProcessLine("loadCrossSections(qcd_files, z_files, w_files,top_files, wprime10_files, wprime15_files, wprime20_files)");
 
-  
+  string outfile("event_counts.txt");
+  ofstream out(outfile.c_str());
+  if(!out) { 
+    cout << "Cannot open file " << outfile << endl; 
+    abort();
+  } 
+
   string dir = "QCD";
   gROOT->ProcessLine("loadInputFiles(dir, qcd_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(qcd_files, fout, dir)");
+  gROOT->ProcessLine("GetDistributionGeneric(qcd_files, fout, dir, out)");
   
   dir = "Z";
   gROOT->ProcessLine("loadInputFiles(dir, z_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(z_files, fout, dir)");
+  gROOT->ProcessLine("GetDistributionGeneric(z_files, fout, dir, out)");
 
   dir = "W";
   gROOT->ProcessLine("loadInputFiles(dir, w_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(w_files, fout, dir)");
+  gROOT->ProcessLine("GetDistributionGeneric(w_files, fout, dir, out)");
   
   dir = "Top";
   gROOT->ProcessLine("loadInputFiles(dir, top_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(top_files, fout, dir)");  
+  gROOT->ProcessLine("GetDistributionGeneric(top_files, fout, dir, out)");  
 
   dir = "wprime10";
   gROOT->ProcessLine("loadInputFiles(dir, wprime10_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(wprime10_files, fout, dir)");  
+  gROOT->ProcessLine("GetDistributionGeneric(wprime10_files, fout, dir, out)"); 
 
   dir = "wprime15";
   gROOT->ProcessLine("loadInputFiles(dir, wprime15_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(wprime15_files, fout, dir)");  
-
+  gROOT->ProcessLine("GetDistributionGeneric(wprime15_files, fout, dir, out)");
+ 
   dir = "wprime20";
   gROOT->ProcessLine("loadInputFiles(dir, wprime20_files, lumiPb)");
-  gROOT->ProcessLine("GetDistributionGeneric(wprime20_files, fout, dir)");  
+  gROOT->ProcessLine("GetDistributionGeneric(wprime20_files, fout, dir, out)"); 
 
+  out.close(); 
   fout->Close();
 }
