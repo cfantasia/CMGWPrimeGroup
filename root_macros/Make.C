5563f9108bb61c420a838d7248243a3ae63bef51
cleonido <>
2010-07-16 21:48:58 +0000

    cleanup makefile

--- root_macros/Make.C
+++ root_macros/Make.C
@@ -2,16 +2,16 @@
 
   gROOT->Reset();
   
+  /*
   // Update the include path so that we can find wprimeEvent.cc
   TString incpath = gSystem->GetIncludePath();
   incpath.Append(" -I$CMSSW_BASE/src");
   gSystem->SetIncludePath(incpath.Data());
-  
+  */
   
   // compile code
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/src/wprimeEvent.cc+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/loadInputFiles.C+");
-  gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/loadCrossSections.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/loadCutsAndThresholds.C+");
   gROOT->ProcessLine(".L UserCode/CMGWPrimeGroup/root_macros/GetDistributionGeneric.C+");
   gROOT->ProcessLine(".x UserCode/CMGWPrimeGroup/root_macros/run.C+");
