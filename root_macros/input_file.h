173ee07114ee4afb195aab7cf6cd1371bfd01197
cleonido <>
2010-07-16 21:48:38 +0000

    added samplename variable (e.g. "W", "QCD", "wprime1.0", etc)

--- root_macros/input_file.h
+++ root_macros/input_file.h
@@ -7,20 +7,30 @@
 class TTree;
 
 namespace wprime{
+  static std::string INVALID = "INVALID";
 
   struct InputFile
   {
     float x_sect; // cross-section in pb
     int Nprod_evt; // # of events produced (before any analysis cuts or other filtering)
     float weight; // cross-section * integrated luminosity / (# of events produced)
+    std::string samplename;
     std::string pathname;
     std::string description; // sample description
     TTree * tree; // pointer to ROOT file
     //
-    InputFile(float cross_sect, int nprod_evt)
+    InputFile()
     {
-      x_sect = cross_sect; Nprod_evt = nprod_evt; weight = 0; tree = 0;
+      x_sect = -1; Nprod_evt = -1; weight = 0; tree = 0;
+      samplename = pathname = description = INVALID;
     }
+    void checkFile()
+    {
+      assert(x_sect > 0); assert(Nprod_evt > 0); assert(weight > 0);
+      assert(pathname != INVALID); assert(description != INVALID); 
+      assert(samplename != INVALID);
+    }
+
   };
 }
 
