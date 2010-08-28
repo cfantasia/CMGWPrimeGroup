5b5a4a935b9ea102cb3ca5f948513e978b75f666
cleonido <>
2010-08-28 21:59:51 +0000

    separate histogram constants that can be used by plot-making macros

--- root_macros/loadCutsAndThresholds.h
+++ root_macros/loadCutsAndThresholds.h
@@ -15,6 +15,7 @@
 #include <sstream>
 
 #include "UserCode/CMGWPrimeGroup/interface/wprimeEvent.h"
+#include "UserCode/CMGWPrimeGroup/root_macros/wprime_histo_constants.h"
 
 
 // $$$$$$$$$$$$$$$$$$$$$$$ Main Study
@@ -29,19 +30,6 @@ const float OneMuPtTrackCut = 20;
 const float Chi2Cut = 10;
 const float Delta_Phi = TMath::Pi() - 0.3;//min angle muon/jet for jet veto
 const float Muon_Eta_Cut = 1.8;
-
-// ++++++++++++++++++++++++++++++++Useful constants
-const int Num_histo_sets = 6; // one new set of histograms after each cut
-const int Num_trkAlgos = 6; // global, tracker, tpfms, cocktail, picky, tmr
-
-// use this for histogram names
-const string algo_desc_short[Num_trkAlgos] = {"gbl","trk","tpfms","ckt","pic","tmr"};
-const string cuts_desc_short[Num_histo_sets] = {"hlt","1mu","ptrange","iso", "jet", "qual"};
-// use this for histogram descriptions
-const string algo_desc_long[Num_trkAlgos] = {"global", "tracker", "TPFMS","cocktail","picky","TMR"};
-const string cuts_desc_long[Num_histo_sets]= {"HLT_Mu9,", "Pt within range,", 
-					     "1 muon only,","Isolation,", 
-					     "Jet Veto,", "Quality,"};
 
 #define debugme  0
 #define debugmemore 0
---
+++ root_macros/wprime_histo_constants.h
@@ -0,0 +1,17 @@
+#ifndef _wprime_histo_constants_h__
+#define _wprime_histo_constants_h__
+
+// ++++++++++++++++++++++++++++++++Useful constants
+const int Num_histo_sets = 6; // one new set of histograms after each cut
+const int Num_trkAlgos = 6; // global, tracker, tpfms, cocktail, picky, tmr
+
+// use this for histogram names
+const string algo_desc_short[Num_trkAlgos] = {"gbl","trk","tpfms","ckt","pic","tmr"};
+const string cuts_desc_short[Num_histo_sets] = {"hlt","1mu","ptrange","iso", "jet", "qual"};
+// use this for histogram descriptions
+const string algo_desc_long[Num_trkAlgos] = {"global", "tracker", "TPFMS","cocktail","picky","TMR"};
+const string cuts_desc_long[Num_histo_sets]= {"HLT_Mu9", "Pt within range", 
+					     "1 muon only","Isolation", 
+					     "Jet Veto", "Quality"};
+
+#endif // #ifndef _wprime_histo_constants_h__
