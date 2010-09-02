7d36c2ac6d81c4730ca2d5763a18f05f9a971fab
cleonido <>
2010-09-02 13:49:15 +0000

    new tag

--- doc/README.txt
+++ doc/README.txt
@@ -9,11 +9,11 @@ UserCode/CMGWPrimeGroup/macros/setup_Wprime.sh
 
 It will:
 
-o Setup a new working area (Default name: V121, release:
+o Setup a new working area (Default name: V132, release:
 CMSSW_3_6_3)
 
 o Check out the UserCode/CMGWPrimeGroup package (default version:
-V00-01-31)
+V00-01-32)
 
 o Compile the code
 
--- macros/setup_Wprime.sh
+++ macros/setup_Wprime.sh
@@ -10,7 +10,7 @@ fi
 
 # Definitions
 RELEASE_VERSION=CMSSW_3_6_3
-WORKING_AREA=V131
+WORKING_AREA=V132
 # end definitions
 
 export RELEASE_VERSION WORKING_AREA
@@ -29,7 +29,7 @@ cd $RELEASE_VERSION/src
 echo -e  "\n**************************"
 echo -e  " Checking out the code..."
 echo -e  "**************************"
-cvs -Q co -r V00-01-31 UserCode/CMGWPrimeGroup
+cvs -Q co -r V00-01-32 UserCode/CMGWPrimeGroup
 # cvs -Q co UserCode/CMGWPrimeGroup
 
 echo -e "\n************************"
