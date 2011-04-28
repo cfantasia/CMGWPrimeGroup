6100deae48e89537df82c6813d4ad5944e070e94
cleonido <>
2011-04-28 10:49:14 +0000

    bugfix: removeMCmatching accidentally enabled in skimming MC cfg files of El+MET analysis

--- test/patTuple_elmet_MC_cfg_lowPtSkim.py
+++ test/patTuple_elmet_MC_cfg_lowPtSkim.py
@@ -4,9 +4,6 @@ from PhysicsTools.PatAlgos.tools.coreTools import *
 from UserCode.CMGWPrimeGroup.patTuple_elmet_cfg import *
 from UserCode.CMGWPrimeGroup.patTuple_mc_cfg import *
 
-## remove MC matching from the default sequence when running on data
-removeMCMatching(process, ['All'])
-
 # 2nd argument: message-logger frequency
 # 3rd argument: # of events to process
 elmet_config(process, 1000, -1)
@@ -35,6 +32,7 @@ process.p = cms.Path(
     process.lowPtElectronFilter *
     ~process.highPtElectronFilter
 )
+
 
 process.source.fileNames = [          ##
     'rfio:/castor/cern.ch/user/c/cleonido/414_Wleptonic/WPlusToENu_CT10_TuneZ2_7TeV_powheg-pythia-4816882C-2D56-E011-B247-0017A4771030.root'
