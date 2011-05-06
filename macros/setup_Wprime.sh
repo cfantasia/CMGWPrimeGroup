98956b513c270ebdedc3bf6b08f06794c20f5d19
cleonido <>
2011-05-06 03:36:06 +0000

    new tag

--- macros/setup_Wprime.sh
+++ macros/setup_Wprime.sh
@@ -10,7 +10,7 @@ fi
 
 # Definitions
 RELEASE_VERSION=CMSSW_4_1_4
-WORKING_AREA=V239
+WORKING_AREA=V250
 # end definitions
 
 export RELEASE_VERSION WORKING_AREA
@@ -33,7 +33,7 @@ echo -e  "**************************"
 cvs -Q co -r $RELEASE_VERSION DataFormats/PatCandidates
 cvs -Q co -r $RELEASE_VERSION PhysicsTools/PatAlgos
 cvs -Q co -r V00-05-00 -d SHarper/HEEPAnalyzer UserCode/SHarper/HEEPAnalyzer 
-cvs -Q co -r V00-02-39 UserCode/CMGWPrimeGroup
+cvs -Q co -r V00-02-50 UserCode/CMGWPrimeGroup
 # cvs -Q co UserCode/CMGWPrimeGroup
 
 echo -e  "\n************************************************************"
