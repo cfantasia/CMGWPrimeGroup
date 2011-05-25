82eac3fa7062b4706250da62b8191c03c79d3c44
cleonido <>
2011-05-25 13:55:34 +0000

    fix typo

--- bin/analyzeWprimeElNu_cfg.py
+++ bin/analyzeWprimeElNu_cfg.py
@@ -5,7 +5,7 @@ import FWCore.ParameterSet.Types as CfgTypes
 process = cms.Process("WPrimeAnalysis")
 # get JSON file correctly parced
 #JSONfile = 'CertUserCode/CMGWPrimeGroup/JSON/_160404-163757_7TeV_PromptReco_Collisions11_JSON.txt'
-JSONfile = 'jsUserCode/CMGWPrimeGroup/JSON/on_160404-163869_DCSonly.txt'
+JSONfile = 'UserCode/CMGWPrimeGroup/JSON/json_160404-163869_DCSonly.txt'
 myList = LumiList.LumiList (filename = JSONfile).getCMSSWString().split(',')
 
 process.inputs = cms.PSet (
