9aecc8ec51bdeb935ee9cd5427d81018f72f9ef0
fantasia <>
2011-06-01 17:02:49 +0000

    Initial Commit

---
+++ interface/BosonFinder.h
@@ -0,0 +1,249 @@
+#ifndef BOSONFINDER_H
+#define BOSONFINDER_H
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include <TROOT.h>
+#include <TH1F.h>
+#include <TVector3.h>
+#include <TFile.h>
+#include <TSystem.h>
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/PatCandidates/interface/TriggerEvent.h"
+#include "DataFormats/Common/interface/MergeableCounter.h"
+#include "DataFormats/Math/interface/deltaR.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenRunInfoProduct.h"
+
+#include "PhysicsTools/SelectorUtils/interface/Selector.h"
+#include "PhysicsTools/CandUtils/interface/AddFourMomenta.h"
+
+#include "UserCode/CMGWPrimeGroup/interface/TeVMuon.h"
+#include "SHarper/HEEPAnalyzer/interface/HEEPEleSelector.h"
+#include "SHarper/HEEPAnalyzer/interface/HEEPEle.h"
+
+
+#include "UserCode/CMGWPrimeGroup/interface/util.h"
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef vector<int> vint;
+typedef vector<string> vstring;
+typedef math::XYZPoint Point;
+typedef math::XYZTLorentzVector LorentzVector;
+typedef edm::ParameterSet PSet;
+typedef edm::MergeableCounter Counter;
+
+//typedef vector<pat::Electron> ElectronV;
+//typedef vector<pat::Muon    > MuonV;
+typedef vector<heep::Ele > ElectronV;
+typedef vector<TeVMuon  > MuonV;
+
+typedef vector<pat::Jet     > JetV;
+typedef vector<pat::MET     > METV;
+typedef vector<reco::Track  > TrackV;
+typedef vector<edm::InputTag> VInputTag;
+typedef vector<reco::Candidate> CandV;
+typedef vector<reco::GenParticle> GenParticleV;
+
+class BosonCandidate : public reco::CompositeCandidate {
+ public:
+  int flavor() const {
+    if (numberOfDaughters() > 0)
+      return abs(daughter(0)->pdgId());
+    return 0;
+  }
+  operator bool() const {
+    return (numberOfDaughters() > 0);
+  }
+ protected:
+  void addDaughters(const reco::Candidate & p1, const reco::Candidate & p2) {
+    addDaughter(p1);
+    addDaughter(p2);
+    AddFourMomenta addP4;
+    addP4.set(* this);
+  }
+  int findGenMotherId(const reco::Candidate * p) const {
+    const reco::Candidate * mom = findMother(p);
+    if (mom) return mom->pdgId();
+    return 0;
+  }
+};
+
+class ZCandidate : public BosonCandidate {
+ public:
+  ZCandidate() {genLepton1_ = genLepton2_ = 0;}
+  ZCandidate(const heep::Ele & p1, const heep::Ele & p2) {
+    genLepton1_ = p1.patEle().genLepton();
+    genLepton2_ = p2.patEle().genLepton();
+    addDaughters(p1.patEle(), p2.patEle());
+  }
+  ZCandidate(const TeVMuon & p1, const TeVMuon & p2) {
+    genLepton1_ = p1.genLepton();
+    genLepton2_ = p2.genLepton();
+    addDaughters(p1, p2);
+  }
+  const reco::Candidate * genLepton1() const {return genLepton1_;}  
+  const reco::Candidate * genLepton2() const {return genLepton2_;}
+  int genMotherId1() const {return findGenMotherId(genLepton1_);}
+  int genMotherId2() const {return findGenMotherId(genLepton2_);}
+ private:
+  const reco::Candidate * genLepton1_;
+  const reco::Candidate * genLepton2_;
+};
+
+class WCandidate : public BosonCandidate {
+ public:
+  WCandidate() {genLepton_ = 0;}
+  WCandidate(const heep::Ele & lepton, const reco::Candidate & met) {
+    genLepton_ = lepton.patEle().genLepton();
+    addDaughters(lepton.patEle(), met);
+  }
+  WCandidate(const TeVMuon & lepton, const reco::Candidate & met) {
+    genLepton_ = lepton.genLepton();
+    addDaughters(lepton, met);
+  }
+  const reco::Candidate * lepton() const {return daughter(0);}
+  const reco::Candidate * met() const {return daughter(1);}
+  const reco::Candidate * genLepton() const {return genLepton_;}
+  int genMotherId() const {return findGenMotherId(genLepton_);}
+  double mt() const {
+    double dphi = 1 - cos(reco::deltaPhi(lepton()->phi(), met()->phi()));
+    return sqrt(2 * met()->et() * lepton()->et() * dphi);
+  }
+ private:
+  const reco::Candidate * genLepton_;
+};
+
+class WZCandidate {
+
+ public:
+
+  WZCandidate() {initialize_();}
+
+  WZCandidate(const ZCandidate & Z, const WCandidate & W) {
+
+    initialize_();
+    const reco::Candidate * wLep  = W.daughter(0);
+    const reco::Candidate * met   = W.daughter(1);
+    const reco::Candidate * zLep1 = Z.daughter(0);
+    const reco::Candidate * zLep2 = Z.daughter(1);
+
+    LorentzVector trilepP4 = zLep1->p4() + zLep2->p4() + wLep->p4();
+    double term1 = sqrt(trilepP4.M2() + pow(trilepP4.pt(),2)) + met->pt();
+    double term2 = (trilepP4 + met->p4()).pt();
+    transMass_ = sqrt(pow(term1, 2) - pow(term2, 2));
+
+    double dPhi         = deltaPhi(wLep->phi(), met->phi());
+    double g            = (WMASS * WMASS / 2. + 
+                           wLep->pt() * met->pt() * cos(dPhi));
+    double a            = - pow(wLep->pt(), 2);
+    double b            = 2 * g * wLep->pz();
+    double c            = pow(g, 2) - pow(wLep->p(), 2) * pow(met->pt(), 2);
+    double discriminant = (b * b) - (4 * a * c);
+    
+    if (discriminant > 0) {
+    
+      TVector3 leptonP3(wLep->px(), wLep->py(), wLep->pz());
+    
+      double   pz1 = -b/(2*a) + sqrt(discriminant)/(2*a);
+      TVector3 p1  = TVector3(met->px(), met->py(), pz1);
+      double   dr1 = p1.DeltaR(leptonP3);
+      
+      double   pz2 = -b/(2*a) - sqrt(discriminant)/(2*a);
+      TVector3 p2  = TVector3(met->px(), met->py(), pz2);
+      double   dr2 = p2.DeltaR(leptonP3);
+
+      neutrinoPz_[0] = (dr1 < dr2) ? pz1 : pz2;
+      neutrinoPz_[1] = (dr1 < dr2) ? pz2 : pz1;
+      neutrinoPz_[2] = (fabs(pz1) > fabs(pz2)) ? pz1 : pz2;
+      neutrinoPz_[3] = (fabs(pz1) > fabs(pz2)) ? pz2 : pz1;
+
+      for (size_t i = 0; i < neutrinoPz_.size(); i++) {
+        double        px = met->px();
+        double        py = met->py();
+        double        pz = neutrinoPz_[i];
+        double        E  = sqrt(pow(px,2) + pow(py,2) + pow(pz,2));
+        LorentzVector p4 = LorentzVector(px, py, pz, E);
+        invariantMass_[i] = (Z.p4() + wLep->p4() + p4).M();
+      }
+
+    }
+
+  }
+
+  double transMass() {return transMass_;}
+  double mt() {return transMass_;}
+
+  double neutrinoPz(std::string type) {
+    return neutrinoPz_[index_(type)];
+  }
+
+  double mass(std::string type) {
+    return invariantMass_[index_(type)];
+  }
+
+ private:
+
+  double transMass_;
+  std::vector<double> neutrinoPz_;
+  std::vector<double> invariantMass_;
+
+  void initialize_() {
+    transMass_ = 0.;
+    neutrinoPz_ = std::vector<double>(4, 0.);
+    invariantMass_ = std::vector<double>(4, 0.);
+  }
+
+  size_t index_(std::string type) {
+    if (type == "minAngle") return 0;
+    if (type == "maxAngle") return 1;
+    if (type == "maxPz"   ) return 2;
+    if (type == "minPz"   ) return 3;
+    printf("Used a non-existent index in WZCandidate\n");
+    return 9;
+  }
+
+};
+
+typedef std::vector<ZCandidate > ZCandV;
+typedef std::vector<WCandidate > WCandV;
+typedef std::vector<WZCandidate> WZCandV;
+
+ZCandV getZCands(const ElectronV & electrons, float maxMassDiff = ZMASS);
+ZCandV getZCands(const MuonV & muons, float maxMassDiff = ZMASS);
+ZCandV getZCands(const ElectronV & electrons,
+                 const MuonV & muons, float maxMassDiff = ZMASS);
+void removeOverlapping(ZCandV & zCands);
+
+WCandidate getWCand(const ElectronV & electrons,
+                    const MuonV & muons, 
+                    const pat::MET & met);
+WCandidate getWCand(const ElectronV & electrons,
+                    const MuonV & muons, 
+                    const pat::MET & met,
+                    const ZCandidate & zCand,
+                    double minDeltaR = 0.);
+WCandidate getWCand(const ElectronV & electrons, 
+                    const pat::MET & met);
+WCandidate getWCand(const MuonV & muons, 
+                    const pat::MET & met);
+
+TVector2 getPtDiff(heep::Ele & e);
+
+TVector2 adjustPt(const ElectronV & electrons);
+TVector2 adjustPt(const MuonV & muons);
+TLorentzVector AdjustedMET(const ElectronV & electrons,
+                           const MuonV & muons,
+                           const pat::MET & met);
+#endif
+
---
+++ src/BosonFinder.cc
@@ -0,0 +1,204 @@
+#include "UserCode/CMGWPrimeGroup/interface/BosonFinder.h"
+
+//////// Useful Algorithms ///////////////////////////////////////////////////
+/// Return a vector of non-overlapping Z candidates
+ZCandV getZCands(const ElectronV & electrons, float maxMassDiff)
+{
+  ZCandV zCands;
+  // Get opposite-charge pairs of electrons
+  for (size_t i = 0; i < electrons.size(); i++)
+    for (size_t j = i + 1; j < electrons.size(); j++)
+      if (electrons[i].charge() != electrons[j].charge())
+        zCands.push_back(ZCandidate(electrons[i], electrons[j]));
+
+  // Order by difference from Z mass
+  sort(zCands.begin(), zCands.end(), closestToZMass());
+
+  removeOverlapping(zCands);
+
+  return zCands;
+}
+
+ZCandV getZCands(const MuonV & muons, float maxMassDiff)
+{
+  ZCandV zCands;
+  // Get opposite-charge pairs of muons
+  for (size_t i = 0; i < muons.size(); i++)
+    for (size_t j = i + 1; j < muons.size(); j++)
+      if (muons[i].charge() != muons[j].charge())
+        zCands.push_back(ZCandidate(muons[i], muons[j]));
+  
+  // Order by difference from Z mass
+  sort(zCands.begin(), zCands.end(), closestToZMass());
+  
+  removeOverlapping(zCands);
+
+  return zCands;
+}
+
+ZCandV getZCands(const ElectronV & electrons, 
+                 const MuonV & muons, float maxMassDiff)
+{
+
+  ZCandV zCands;
+
+  ZCandV zeeCands = getZCands(electrons, maxMassDiff);
+  zCands.insert(zCands.end(), zeeCands.begin(), zeeCands.end());
+
+  ZCandV zmmCands = getZCands(    muons, maxMassDiff);
+  zCands.insert(zCands.end(), zmmCands.begin(), zmmCands.end());
+
+  // Order by difference from Z mass
+  sort(zCands.begin(), zCands.end(), closestToZMass());
+
+  removeOverlapping(zCands);
+
+  return zCands;
+}
+
+void removeOverlapping(ZCandV & zCands){
+  // Get rid of overlapping candidates
+  for (ZCandV::iterator i = zCands.begin(); i != zCands.end(); ++i)
+    for (ZCandV::iterator j = i + 1; j != zCands.end(); ++j)
+      if (areOverlapping(*i, *j)) {
+        zCands.erase(j);
+        j = i;
+      }
+}
+
+WCandidate getWCand(const ElectronV & electrons,
+                    const pat::MET & met){
+  vector<WCandidate> wCands;
+  
+  for (ElectronV::const_iterator i = electrons.begin(); 
+       i != electrons.end(); ++i)
+    wCands.push_back(WCandidate(* i, met));
+
+  sort(wCands.begin(), wCands.end(), highestPtLepton());
+
+  if (wCands.size()) return wCands[0];
+
+  return WCandidate();
+
+}
+
+WCandidate getWCand(const MuonV & muons, 
+                    const pat::MET & met){
+  vector<WCandidate> wCands;
+  for (MuonV::const_iterator i = muons.begin(); 
+       i != muons.end(); ++i)
+    wCands.push_back(WCandidate(* i, met));
+
+  sort(wCands.begin(), wCands.end(), highestPtLepton());
+
+  if (wCands.size()) return wCands[0];
+
+  return WCandidate();
+}
+
+WCandidate getWCand(const ElectronV & electrons,
+                    const MuonV & muons, 
+                    const pat::MET & met){
+ vector<WCandidate> wCands;
+
+ wCands.push_back(getWCand(electrons, met));
+ wCands.push_back(getWCand(muons    , met));
+
+ sort(wCands.begin(), wCands.end(), highestPtLepton());
+ 
+ if (wCands.size()) return wCands[0];
+ 
+ return WCandidate();
+}
+
+/// Return a WCandidate using the highest-pT non-Z lepton
+WCandidate getWCand(const ElectronV & electrons,
+                    const MuonV & muons, 
+                    const pat::MET & met,
+                    const ZCandidate & zCand,
+                    double minDeltaR)
+{
+  if (!zCand) return WCandidate();
+
+  vector<WCandidate> wCands;
+
+  for (ElectronV::const_iterator i = electrons.begin(); 
+       i != electrons.end(); ++i)
+    if (!areOverlapping(i->patEle(), * zCand.daughter(0)) &&
+        !areOverlapping(i->patEle(), * zCand.daughter(1)) &&
+        reco::deltaR(* i, * zCand.daughter(0)) > minDeltaR &&
+        reco::deltaR(* i, * zCand.daughter(1)) > minDeltaR) {
+      wCands.push_back(WCandidate(* i, met));
+    }
+
+  for (MuonV::const_iterator i = muons.begin(); 
+       i != muons.end(); ++i)
+    if (!areOverlapping(* i, * zCand.daughter(0)) &&
+        !areOverlapping(* i, * zCand.daughter(1)) &&
+        reco::deltaR(* i, * zCand.daughter(0)) > minDeltaR &&
+        reco::deltaR(* i, * zCand.daughter(1)) > minDeltaR) {
+      wCands.push_back(WCandidate(* i, met));
+    }
+
+  sort(wCands.begin(), wCands.end(), highestPtLepton());
+
+  if (wCands.size()) return wCands[0];
+
+  return WCandidate();
+}
+
+
+
+/// Return candidates using the highest-pT non-Z lepton and each of the METs
+WCandV getWCands(const ElectronV & electrons, 
+                 const MuonV & muons, 
+                 const METV & mets, 
+                 const ZCandidate & zCand,
+                 double minDeltaR = 0.)
+{
+  WCandV wCands;
+  for (size_t i = 0; i < mets.size(); i++) {
+    WCandidate wCand = getWCand(electrons, muons, mets[i], zCand, minDeltaR);
+    if (wCand) wCands.push_back(wCand);
+  }
+  return wCands;
+}
+
+TVector2 getPtDiff(const heep::Ele & e){ 
+  TVector2  chosenAlgo( e.p4().px(), e.p4().py() );
+  LorentzVector p4Def = e.patEle().p4();
+  TVector2 defaultAlgo( p4Def.Px(), p4Def.Py() );
+  return chosenAlgo - defaultAlgo;
+}
+
+TVector2
+adjustPt(const ElectronV & electrons){
+  TVector2 diff(0.,0.);
+  for (ElectronV::const_iterator i = electrons.begin(); 
+       i != electrons.end(); ++i){
+    diff = diff + getPtDiff(*i);
+  }
+  return diff;
+}
+
+TVector2
+adjustPt(const MuonV & muons){
+  TVector2 diff(0.,0.);
+  for (MuonV::const_iterator i = muons.begin(); 
+       i != muons.end(); ++i){
+    diff = diff + i->getPtDiff();
+  }
+  return diff;
+}
+
+TLorentzVector
+AdjustedMET(const ElectronV & electrons,
+            const MuonV & muons,
+            const pat::MET & met){
+  TVector2 met2(met.px(), met.py());
+
+  TVector2 totalAdj = adjustPt(electrons) + adjustPt(muons);
+  
+  //How are we going to do this part!!!
+  return TLorentzVector(met2.Px(), met2.Py(), 0, met2.Mod());
+}
