52f1bb12420c10d41d2df658675685286fe139bb
fantasia <>
2011-06-09 12:03:52 +0000

    Int Lumi now separated from data sample

--- src/WPrimeUtil.cc
+++ src/WPrimeUtil.cc
@@ -86,34 +86,35 @@ void WPrimeUtil::getInputFiles(std::vector<wprime::InputFile> & inputFiles)
   ifstream in(txt_file.c_str());
   string new_line; wprime::InputFile * new_file = 0;
   while(getline(in, new_line))
-    {
-      // if DONE, we are done!
-      if(new_line == "DONE")break;
-
-      if(new_line.find("samplename = ") != string::npos)
-	// new file found! create structure to put in info
-	new_file = new wprime::InputFile();
-      
-      if(!new_line.empty())
-	parseLine(new_line, new_file);
-      else
-	{
-	  if(new_file->samplename.find("data") == string::npos)
-	    new_file->weight = lumi_ipb*(new_file->x_sect)
-	      /(new_file->Nprod_evt);
-	  else
-	    new_file->weight = 1;
-	  cout << " Weight to be applied on " << new_file->samplename
-	       << " sample = " << new_file->weight << endl << endl;
-	  // all info should now be logged in; check!
-	  new_file->checkFile();
-	  // if we made it here, everything looks good: 
-	  // add to vector of input files
-	  inputFiles.push_back(*new_file);
-	  // release memory
-	  delete new_file;
-	}
+  {
+    // if DONE, we are done!
+    if(new_line == "DONE")break;
+    
+    if(new_line.find("samplename = ") != string::npos)
+      // new file found! create structure to put in info
+      new_file = new wprime::InputFile();
+    cout<<"TESTING\n";
+    if(!new_line.empty())
+      parseLine(new_line, new_file);
+    else{ //Empty line means the end of a sample
+      if(new_file){
+        if(new_file->samplename.find("data") == string::npos)
+          new_file->weight = lumi_ipb*(new_file->x_sect)
+            /(new_file->Nprod_evt);
+        else
+          new_file->weight = 1;
+        cout << " Weight to be applied on " << new_file->samplename
+             << " sample = " << new_file->weight << endl << endl;
+        // all info should now be logged in; check!
+        new_file->checkFile();
+      // if we made it here, everything looks good: 
+      // add to vector of input files
+        inputFiles.push_back(*new_file);
+        // release memory
+        delete new_file;
+      }
     }
+  }
 }
 
 
@@ -131,6 +132,29 @@ void WPrimeUtil::getEff(float & eff, float & deff,float Num,float Denom)
 void WPrimeUtil::parseLine(const string & new_line, wprime::InputFile * in_file)
 {
   size_t i = 0;
+  // this is where we extract the integrated luminosity
+  if(lumi_ipb < 0)
+	{
+    i = new_line.find("integrated lumi = ");
+    if(i != string::npos)
+    {
+      unsigned int begin = new_line.find("=") + 2;
+      unsigned int end = new_line.find("pb^-1");
+      string integ_lumi = new_line.substr(begin, end-begin);
+      lumi_ipb = atof(integ_lumi.c_str());
+      
+      cout << "\n Will apply weights to MC samples to get distributions for "
+           << lumi_ipb << " pb^-1" << endl << endl;
+      if(lumi_ipb <= 0)
+      {
+        cout << " *** Oops, read in \"" << integ_lumi 
+             << "\" which I failed to parse! Please seek help... " 
+             << endl;
+        abort();
+      }
+    }
+  }
+
   i = new_line.find("samplename = ");
   if(i != string::npos)
     {
@@ -142,28 +166,7 @@ void WPrimeUtil::parseLine(const string & new_line, wprime::InputFile * in_file)
   if(i != string::npos)
     {
       in_file->description = new_line.substr(14, new_line.length() - 14);
-
-      // this is where we extract the integrated luminosity
-      // from the description of the data sample; expect something like
-      // "blah-blah-blah random description, XYZ pb^-1" ==> XYZ is what we want
-      // NB: the data sample must appear first in samples_cross_sections.txt !
-      if(lumi_ipb < 0)
-	{
-	  unsigned int begin = in_file->description.find(",") + 2;
-	  unsigned int end = in_file->description.find("pb^-1");
-	  string integ_lumi = in_file->description.substr(begin, end-begin);
-	  lumi_ipb = atof(integ_lumi.c_str());
-
-	  cout << "\n Will apply weights to MC samples to get distributions for "
-	       << lumi_ipb << " pb^-1" << endl << endl;
-	  if(lumi_ipb <= 0)
-	    {
-	      cout << " *** Oops, read in \"" << integ_lumi 
-		   << "\" which I failed to parse! Please seek help... " 
-		   << endl;
-	      abort();
-	    }
-	}
+      
       return;
     }
 
