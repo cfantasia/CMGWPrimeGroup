faddba67abb58ad960adcb520dfc499898d8c093
cleonido <>
2011-07-29 13:57:29 +0000

    implemeted WP80 cuts

--- macros/setup_Wprime.sh
+++ macros/setup_Wprime.sh
@@ -10,7 +10,7 @@ fi
 
 # Definitions
 RELEASE_VERSION=CMSSW_4_2_3
-WORKING_AREA=V280
+WORKING_AREA=V290
 # end definitions
 
 export RELEASE_VERSION WORKING_AREA
@@ -34,12 +34,13 @@ cvs -Q co -r $RELEASE_VERSION DataFormats/PatCandidates
 cvs -Q co -r $RELEASE_VERSION PhysicsTools/PatAlgos
 cvs -Q co -r V00-05-00 -d SHarper/HEEPAnalyzer UserCode/SHarper/HEEPAnalyzer 
 cvs -Q co -r V08-03-03 PhysicsTools/Utilities
-cvs -Q co -r V00-02-80 UserCode/CMGWPrimeGroup
+cvs -Q co -r V00-02-90 UserCode/CMGWPrimeGroup
 # cvs -Q co UserCode/CMGWPrimeGroup
 
 echo -e  "\n************************************************************"
 echo -e  " Hack PAT Muon content to include high-pt reconstructors..."
 echo -e  " Fix BuildFiles (ie. make 41x-compatible) for HEEP code..."
+echo -e  " Add WP80 selection to HEEP code...."
 echo -e  "************************************************************"
 mv DataFormats/PatCandidates/interface/Muon.h DataFormats/PatCandidates/interface/Muon.h_original
 cp UserCode/CMGWPrimeGroup/PAT_hack/Muon.h DataFormats/PatCandidates/interface/
@@ -53,7 +54,19 @@ mv PhysicsTools/PatAlgos/python/producersLayer1/muonProducer_cfi.py PhysicsTools
 cp UserCode/CMGWPrimeGroup/PAT_hack/muonProducer_cfi.py PhysicsTools/PatAlgos/python/producersLayer1/
 cp UserCode/CMGWPrimeGroup/SHarper_hack/BuildFile1.xml SHarper/HEEPAnalyzer/BuildFile.xml
 cp UserCode/CMGWPrimeGroup/SHarper_hack/BuildFile2.xml SHarper/HEEPAnalyzer/plugins/BuildFile.xml
-
+cp UserCode/CMGWPrimeGroup/SHarper_hack/WP80SelectionCuts_cfi.py SHarper/HEEPAnalyzer/python/
+mv SHarper/HEEPAnalyzer/src/HEEPEleSelector.cc SHarper/HEEPAnalyzer/src/HEEPEleSelector.cc_original
+cp UserCode/CMGWPrimeGroup/SHarper_hack/HEEPEleSelector.cc SHarper/HEEPAnalyzer/src/
+mv SHarper/HEEPAnalyzer/interface/HEEPEle.h SHarper/HEEPAnalyzer/interface/HEEPEle.h_original
+cp UserCode/CMGWPrimeGroup/SHarper_hack/HEEPEle.h SHarper/HEEPAnalyzer/interface/ 
+mv SHarper/HEEPAnalyzer/interface/HEEPCutValues.h  SHarper/HEEPAnalyzer/interface/HEEPCutValues.h_original
+cp UserCode/CMGWPrimeGroup/SHarper_hack/HEEPCutValues.h SHarper/HEEPAnalyzer/interface/
+mv SHarper/HEEPAnalyzer/src/HEEPCutValues.cc  SHarper/HEEPAnalyzer/src/HEEPCutValues.cc_original
+cp UserCode/CMGWPrimeGroup/SHarper_hack/HEEPCutValues.cc  SHarper/HEEPAnalyzer/src/
+mv SHarper/HEEPAnalyzer/interface/HEEPCutCodes.h SHarper/HEEPAnalyzer/interface/HEEPCutCodes.h_original
+cp UserCode/CMGWPrimeGroup/SHarper_hack/HEEPCutCodes.h SHarper/HEEPAnalyzer/interface/
+mv SHarper/HEEPAnalyzer/src/HEEPCutCodes.cc SHarper/HEEPAnalyzer/src/HEEPCutCodes.cc_original
+cp UserCode/CMGWPrimeGroup/SHarper_hack/HEEPCutCodes.cc SHarper/HEEPAnalyzer/src/
 echo -e "\n************************"
 echo -e " Done. Now will compile"
 echo -e "************************"
