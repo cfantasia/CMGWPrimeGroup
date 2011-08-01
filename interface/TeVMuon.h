5ffda2de4fcd7992ed3d866c3ad0847efddf1ba7
fantasia <>
2011-08-01 14:29:52 +0000

    Upgraded, simplified class

--- interface/TeVMuon.h
+++ interface/TeVMuon.h
@@ -8,16 +8,42 @@
 
 class TeVMuon : public pat::Muon{
  public:
-  TeVMuon(const pat::Muon & muon, unsigned int muReconstructor=kTEV, bool isValid=1) :
-    pat::Muon(muon), muReconstructor_(muReconstructor) {p4_ = p4(isValid_);}
+  TeVMuon(const reco::Muon & muon, const unsigned muReconstructor=kTEV, bool isValid=1) :
+    pat::Muon(muon), muReconstructor_(muReconstructor),isValid_(isValid) {setMuLorentzVector(muReconstructor);}
+  TeVMuon(const pat::Muon & muon, const unsigned muReconstructor=kTEV, bool isValid=1) :
+    pat::Muon(muon), muReconstructor_(muReconstructor),isValid_(isValid) {setMuLorentzVector(muReconstructor);}
   ~TeVMuon(){}
   
-  // get muon 4-d momentum according to muonReconstructor_ value
-  const TLorentzVector & p4(unsigned muReconstructor, 
-                            bool & isInvalidMuon);
+  //////////////////////////////
+  ///TeV Accessor Functions/////
+  //////////////////////////////
+  const unsigned getmuReconstructor() const;
+  const bool isValid() const;
+  const bool isValid(const unsigned muReconstructor);
+  const reco::TrackRef getTrack(const unsigned muReconstructor) const;
+  const TLorentzVector getTrkLorentzVector(const reco::TrackRef trk) const;
 
-  // isInvalidMuon: set to false if muon track (for given reconstructor) is NULL
-  const TLorentzVector & p4(bool & isInvalidMuon);
+  //const TLorentzVector P4() const;
+  const TLorentzVector p4(const unsigned muReconstructor) const;
+  double pt() const ;
+  const double pt(const unsigned muReconstructor) const ;
+
+  //////////////////////////////
+  ///TeV Helper Functions///////
+  //////////////////////////////
+
+  TVector2 getPtDiff() const;
+
+  // computes the tracker-based relative isolation value
+  float trkRelIsolation() const;
+
+  //computes the combined rel isolation value
+  float combRelIsolation() const;
+  float combRelIsolation03(const float offset) const;
+  
+//////////////////////////////
+///TeV Cut Functions//////////
+//////////////////////////////
 
   bool goodQualityMuon(float chi2Cut, float muonEtaCut) const;
   bool PassEtaCut(const float cut);
@@ -32,27 +58,146 @@ class TeVMuon : public pat::Muon{
   bool PassStationsCut(const float cut);
   bool PassCombRelIsoCut(const float cut);
   bool PassCombRelIso03Cut(const float cut, const float puOffset=0.);
-
-  double pt() const ;
-  double pt(unsigned muReconstructor) const ;
-  // computes the tracker-based relative isolation value
-  float trkRelIsolation() const;
-  //computes the combined rel isolation value
-  float combRelIsolation() const;
-  float combRelIsolation03(const float offset) const;
   
-  TVector2 getPtDiff() const;
-  const reco::TrackRef GetTrack(unsigned muReconstructor) const;
-  const TLorentzVector getTrkLorentzVector(unsigned muReconstructor) const;
-  const TLorentzVector getTrkLorentzVector(const reco::TrackRef & trk, bool & isInvalidMuon) const;
-
  private:
+  unsigned muReconstructor_;
   bool isValid_;
-  unsigned int muReconstructor_;
-  TLorentzVector p4_;
 
-  void setMuLorentzVector(const reco::TrackRef & trk, bool & isInvalidMuon);
-
+  //////////////////////////////
+  ///TeV Modifier Functions/////
+  //////////////////////////////
+  void setMuLorentzVector(const TLorentzVector& p4);
+  void setMuLorentzVector(const reco::TrackRef & trk);
+  void setMuLorentzVector(const unsigned muReconstructor);
+  void setisValid(const bool val);
 };
+
+//////////////////////////////
+///TeV Accessor Functions/////
+//////////////////////////////
+
+inline const unsigned TeVMuon::getmuReconstructor() const {
+  return muReconstructor_;
+}
+
+inline const bool TeVMuon::isValid() const {
+  return isValid_;
+}
+
+inline const bool
+TeVMuon::isValid(const unsigned muReconstructor){
+  isValid_ = !(getTrack(muReconstructor).isNull());
+  return isValid_;
+}
+/*
+inline const TLorentzVector TeVMuon::P4() const{
+  return TLorentzVector(px(), py(), pz(), energy());
+}
+*/
+inline const TLorentzVector TeVMuon::p4(const unsigned muReconstructor) const{
+  return getTrkLorentzVector(getTrack(muReconstructor));
+}
+
+inline double TeVMuon::pt() const{
+  return pat::Muon::pt();
+}
+
+inline const double TeVMuon::pt(const unsigned muReconstructor) const {
+  return p4(muReconstructor).Pt();
+}
+
+//////////////////////////////
+///TeV Helper Functions///////
+//////////////////////////////
+
+// computes the tracker-based relative isolation value
+inline float TeVMuon::trkRelIsolation() const
+{
+  return trackIso()/pt();
+}
+
+//computes the combined rel isolation value
+inline float TeVMuon::combRelIsolation() const
+{
+  return ( ecalIso() + hcalIso() + trackIso() ) 
+    / pt();
+}
+
+inline float TeVMuon::combRelIsolation03(const float offset) const{
+  return (isolationR03().emEt + isolationR03().hadEt + isolationR03().sumPt - offset)
+    / pt();
+}
+
+//////////////////////////////
+///TeV Cut Functions//////////
+//////////////////////////////
+
+inline bool TeVMuon::PassIsGlobalCut(){
+  return isGlobalMuon(); 
+}//--- PassIsGlobalCut
+
+inline bool TeVMuon::PassIsTrackerCut(){
+  return isTrackerMuon(); 
+}//--- PassIsTrackerCut
+
+inline bool TeVMuon::PassPtCut(const float cut){
+  return pt() > cut;
+}
+
+inline bool TeVMuon::PassEtaCut(const float cut){
+  return fabs(eta()) < cut;
+}//--- PassEta Cut
+
+inline bool TeVMuon::PassDxyCut(const float cut){
+  return (fabs(dB()) < cut);
+}//--- PassDxyCut
+
+inline bool TeVMuon::PassNPixHitCut(const float cut){
+  return (globalTrack()->hitPattern().numberOfValidPixelHits() > cut);
+}//--- PassNpixhitCut
+
+inline bool TeVMuon::PassNTrkHitCut(const float cut){
+  return (globalTrack()->hitPattern().numberOfValidTrackerHits() > cut);
+}//--- PassNtrkhitCut
+
+inline bool TeVMuon::PassNormChi2Cut(const float cut){
+  return (globalTrack()->normalizedChi2() < cut);
+}//--- PassChi2Cut
+
+inline bool TeVMuon::PassHitsUsedCut(const float cut){
+  return (globalTrack()->hitPattern().numberOfValidMuonHits() > cut);
+}//--- PassHits Used Cut
+
+inline bool TeVMuon::PassStationsCut(const float cut){
+  return numberOfMatches() > cut;
+}//--- PassStationsCut
+
+inline bool TeVMuon::PassCombRelIso03Cut(const float cut, const float puOffset){
+  return combRelIsolation03(puOffset) < cut;
+}//--- PassCombRelIsoCut
+
+//////////////////////////////
+///TeV Modifier Functions/////
+//////////////////////////////
+
+inline void TeVMuon::setMuLorentzVector(const TLorentzVector& p4)
+{
+  setP4(LorentzVector(p4.Px(), p4.Py(), p4.Pz(), p4.E()));
+}
+
+inline void TeVMuon::setMuLorentzVector(const reco::TrackRef & trk)
+{
+  setMuLorentzVector(getTrkLorentzVector(trk));
+}
+
+inline void TeVMuon::setMuLorentzVector(const unsigned muReconstructor)
+{
+  if(isValid(muReconstructor))
+    setMuLorentzVector(getTrack(muReconstructor));
+}
+
+inline void TeVMuon::setisValid(const bool val){
+  isValid_ = val;
+}
 
 #endif // #define _TeVMuon_h_
--- src/TeVMuon.cc
+++ src/TeVMuon.cc
@@ -1,22 +1,18 @@
 #include "UserCode/CMGWPrimeGroup/interface/TeVMuon.h"
 #include "UserCode/CMGWPrimeGroup/interface/util.h"
 
+//////////////////////////////
+///TeV Modifier Functions/////
+//////////////////////////////
 
-
-// get muon 4-d momentum according to muonReconstructor_ value
-// see interface/TeVMuon.h
-
-const TLorentzVector & TeVMuon::p4(unsigned muReconstructor,
-                                   bool & isInvalidMuon){
-  setMuLorentzVector(GetTrack(muReconstructor), isInvalidMuon);
- 
-  return p4_;
-}
+//////////////////////////////
+///TeV Accessor Functions/////
+//////////////////////////////
 
 const reco::TrackRef
-TeVMuon::GetTrack(unsigned muReconstructor) const{
+TeVMuon::getTrack(const unsigned muReconstructor) const{
   switch(muReconstructor)
-  {
+  {// see interface/TeVMuon.h
   case kGLOBAL:
     return globalTrack();
   case kINNER:
@@ -36,63 +32,29 @@ TeVMuon::GetTrack(unsigned muReconstructor) const{
   return defaultTeVMuon();
 }
 
-double TeVMuon::pt(unsigned muReconstructor) const {
-  return getTrkLorentzVector(muReconstructor).Pt();
-}
-
-TVector2 TeVMuon::getPtDiff() const {
-  TVector2  chosenAlgo( p4_.Px(), p4_.Py() );
-  bool isInvalid = false;
-  TLorentzVector p4Def = getTrkLorentzVector(kGLOBAL);
-  if(isInvalid){
-    std::cout<<"Failed getting muon\n";
-    return TVector2();
-  }
-  TVector2 defaultAlgo( p4Def.Px(), p4Def.Py() );
-  return chosenAlgo - defaultAlgo;
-}
-
-void TeVMuon::setMuLorentzVector(const reco::TrackRef & trk, bool & isInvalidMuon)
-{
-  p4_ = getTrkLorentzVector(trk, isInvalidMuon);
-  setP4(LorentzVector(p4_.Px(), p4_.Py(), p4_.Pz(), p4_.E()));
-}
-
-const TLorentzVector TeVMuon::getTrkLorentzVector(unsigned muReconstructor) const{
-  bool isInvalidMuon = false;
-  return getTrkLorentzVector(GetTrack(muReconstructor), isInvalidMuon);
-}
-
-const TLorentzVector TeVMuon::getTrkLorentzVector(const reco::TrackRef & trk, bool & isInvalidMuon) const
+const TLorentzVector TeVMuon::getTrkLorentzVector(const reco::TrackRef trk) const
 {
   TLorentzVector trkP4;
-  if(trk.isNull()){
-    isInvalidMuon = true;
-  }else{
-    TVector3 p3(trk->px(), trk->py(), trk->pz());
-    trkP4 = TLorentzVector(p3, sqrt(p3.Mag2() + wprime::MUON_MASS*wprime::MUON_MASS));
-    isInvalidMuon = false;
+  if(!trk.isNull()){
+    trkP4.SetXYZM(trk->px(), trk->py(), trk->pz(), wprime::MUON_MASS);
   } 
   return trkP4;
 }
 
-// computes the tracker-based relative isolation value
-float TeVMuon::trkRelIsolation() const
-{
-  return trackIso()/pt();
+//////////////////////////////
+///TeV Helper Functions///////
+//////////////////////////////
+
+TVector2 TeVMuon::getPtDiff() const {
+  TVector2  chosenAlgo( px(), py() );
+  const TLorentzVector& p4Def = p4(kGLOBAL);
+  TVector2 defaultAlgo( p4Def.Px(), p4Def.Py() );
+  return chosenAlgo - defaultAlgo;
 }
 
-//computes the combined rel isolation value
-float TeVMuon::combRelIsolation() const
-{
-  return ( ecalIso() + hcalIso() + trackIso() ) 
-    / pt();
-}
-
-float TeVMuon::combRelIsolation03(const float offset) const{
-  return (isolationR03().emEt + isolationR03().hadEt + isolationR03().sumPt - offset)
-    / pt();
-}
+//////////////////////////////
+///TeV Cut Functions//////////
+//////////////////////////////
 
 bool TeVMuon::goodQualityMuon(float chi2Cut, float muonEtaCut) const
 {
@@ -127,48 +89,4 @@ bool TeVMuon::goodQualityMuon(float chi2Cut, float muonEtaCut) const
   return true;
 }
 
-//////////////////
-inline bool TeVMuon::PassIsGlobalCut(){
-  return isGlobalMuon(); 
-}//--- PassIsGlobalCut
-
-inline bool TeVMuon::PassIsTrackerCut(){
-  return isTrackerMuon(); 
-}//--- PassIsTrackerCut
-
-inline bool TeVMuon::PassPtCut(const float cut){
-  return pt() > cut;
-}
-
-inline bool TeVMuon::PassEtaCut(const float cut){
-  return fabs(eta()) < cut;
-}//--- PassEta Cut
-
-inline bool TeVMuon::PassDxyCut(const float cut){
-  return (fabs(dB()) < cut);
-}//--- PassDxyCut
-
-inline bool TeVMuon::PassNPixHitCut(const float cut){
-  return (globalTrack()->hitPattern().numberOfValidPixelHits() > cut);
-}//--- PassNpixhitCut
-
-inline bool TeVMuon::PassNTrkHitCut(const float cut){
-  return (globalTrack()->hitPattern().numberOfValidTrackerHits() > cut);
-}//--- PassNtrkhitCut
-
-inline bool TeVMuon::PassNormChi2Cut(const float cut){
-  return (globalTrack()->normalizedChi2() < cut);
-}//--- PassChi2Cut
-
-inline bool TeVMuon::PassHitsUsedCut(const float cut){
-  return (globalTrack()->hitPattern().numberOfValidMuonHits() > cut);
-}//--- PassHits Used Cut
-
-inline bool TeVMuon::PassStationsCut(const float cut){
-  return numberOfMatches() > cut;
-}//--- PassStationsCut
-
-inline bool TeVMuon::PassCombRelIso03Cut(const float cut, const float puOffset){
-  return combRelIsolation03(puOffset) < cut;
-}//--- PassCombRelIsoCut
 
