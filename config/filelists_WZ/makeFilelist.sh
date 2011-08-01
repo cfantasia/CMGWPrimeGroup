04c791475a610d777c602ee05b00936a24f3de75
fantasia <>
2011-08-01 14:34:17 +0000

    updated for wisc

--- config/filelists_WZ/makeFilelist.sh
+++ config/filelists_WZ/makeFilelist.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-Ver="05-07-01"
+Ver="W05-08-02"
+Dir=/hdfs/store/user/jklukas
 #Dir=/pnfs/cms/WAX/11/store/user/fantasia/42X
-Dir=~/nobackup/42X/filelists
+#Dir=~/nobackup/42X/filelists
 
-for Directory in `ls ${Dir} | grep ${Ver}`
+for Directory in `ls ${Dir} | grep ${Ver} | grep PatTuple`
   do
   if [ -d $Dir/$Directory ]; then
       find ${Dir}/${Directory}/ | grep .root >& ${Directory}.txt
