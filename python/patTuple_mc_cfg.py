fdbc77e0c381e17060c56475d58f0d11463a358d
fantasia <>
2011-09-06 20:45:18 +0000

    Added Z info

--- python/patTuple_mc_cfg.py
+++ python/patTuple_mc_cfg.py
@@ -7,7 +7,7 @@ def mc_config(process, cms):
            #keeps all particles from the hard matrix element
            "keep status = 3",
            #keeps all stable muons (13) and electrons (11) + neutrinos (14, 12)
-           # + W (24) + W' (34)  and their (direct) mothers.
-           "+keep (abs(pdgId) = 11 | abs(pdgId) = 13 | abs(pdgId) = 12 | abs(pdgId) = 14 | abs(pdgId) = 24 | abs(pdgId) = 34) & status = 1"
+           # + Z (23) + W (24) + W' (34)  and their (direct) mothers.
+           "+keep (abs(pdgId) = 11 | abs(pdgId) = 13 | abs(pdgId) = 12 | abs(pdgId) = 14 | abs(pdgId) = 23 | abs(pdgId) = 24 | abs(pdgId) = 34) & status = 1"
     )
 )
