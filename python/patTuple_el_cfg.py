411c4471d4d1565e0863482531e0bd982d7e1908
cleonido <>
2011-09-08 20:25:21 +0000

    revamp basic electron configuration

--- python/patTuple_el_cfg.py
+++ python/patTuple_el_cfg.py
@@ -1,13 +1,19 @@
 from UserCode.CMGWPrimeGroup.patTuple_common_cfg import *
 
 def el_config(process) :
-    # keep all events with electron-pt above 25 GeV
-    process.lowPtElectronFilter = cms.EDFilter("CandViewCountFilter",
-                                           src = cms.InputTag("selectedPatElectrons"),
-                                           minNumber = cms.uint32(1),
-                                           cut = cms.string( "pt > 10. & abs(eta) < 2.5" )
-                                           )
+    # event content to include all electrons within |eta|<2.5 with pt>10
+     process.selectedPatElectrons.cut = "pt > 10. & abs(eta) < 2.5"
     
     process.selectedPatPFParticles.cut = "abs(pdgId())==11"
     process.out.outputCommands.append('keep *_selectedPatElectrons_*_*')
     
+    # define filter with electron-pt above 100 GeV
+    # NB: it is selectedPatElectrons that are saved in the event!
+    # NB: highPtElectrons is only used for filtering when invoked
+    process.highPtElectrons = process.selectedPatElectrons.clone()
+    process.highPtElectrons.cut = "pt > 100. & abs(eta) < 2.5"
+    
+    process.highPtMuonFilter = cms.EDFilter("CandViewCountFilter",
+                                            src = cms.InputTag("highPtElectrons"),
+                                            minNumber = cms.uint32(1)
+                                            )
