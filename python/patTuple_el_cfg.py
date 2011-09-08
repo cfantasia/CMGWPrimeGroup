39d6ae51e0bf0385a48524a0a6dffab8205d92c6
cleonido <>
2011-09-08 20:45:10 +0000

    common threshold for electrons & muons

--- python/patTuple_el_cfg.py
+++ python/patTuple_el_cfg.py
@@ -1,8 +1,8 @@
 from UserCode.CMGWPrimeGroup.patTuple_common_cfg import *
 
 def el_config(process) :
-    # event content to include all electrons within |eta|<2.5 with pt>10
-     process.selectedPatElectrons.cut = "pt > 10. & abs(eta) < 2.5"
+    # event content to include all electrons within |eta|<2.5 with pt>20
+     process.selectedPatElectrons.cut = "pt > 20. & abs(eta) < 2.5"
     
     process.selectedPatPFParticles.cut = "abs(pdgId())==11"
     process.out.outputCommands.append('keep *_selectedPatElectrons_*_*')
