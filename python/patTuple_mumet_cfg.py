14a74fedb010c89c18782933d005baf3945e51ae
cleonido <>
2011-09-08 20:26:03 +0000

    move stuff to patTuple_mu_cfg

--- python/patTuple_mumet_cfg.py
+++ python/patTuple_mumet_cfg.py
@@ -1,9 +1,11 @@
 from UserCode.CMGWPrimeGroup.patTuple_common_cfg import *
+from UserCode.CMGWPrimeGroup.patTuple_mu_cfg import *
+from UserCode.CMGWPrimeGroup.patTuple_met_cfg import *
 
 def mumet_config(process, reportEveryNum=100, maxEvents=-1) :
     common_config(process, reportEveryNum, maxEvents)
-    process.selectedPatPFParticles.cut = "abs(pdgId())==13"
+    mu_config(process)
+    met_config(process)
 
-    # RECO
-    process.out.outputCommands.append('keep *_selectedPatMuons_*_*')
-    process.out.outputCommands.append('keep *_patMETs*_*_*')
+
+
