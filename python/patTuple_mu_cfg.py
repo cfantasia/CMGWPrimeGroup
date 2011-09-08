b086fed5da9c05e69a897e173747d566db910aa3
cleonido <>
2011-09-08 20:25:31 +0000

    revamp basic muon configuration

--- python/patTuple_mu_cfg.py
+++ python/patTuple_mu_cfg.py
@@ -1,12 +1,20 @@
 from UserCode.CMGWPrimeGroup.patTuple_common_cfg import *
 
 def mu_config(process) :
-    process.lowPtMuonFilter = cms.EDFilter("CandViewCountFilter",
-                                           src = cms.InputTag("selectedPatMuons"),
-                                           minNumber = cms.uint32(1),
-                                           cut = cms.string( "pt > 10. & abs(eta) < 2.5" )
-                                           )
-    
+
+    # event content to include all muons within |eta|<2.4 with pt>20
+    process.selectedPatMuons.cut = "pt > 20. & abs(eta) < 2.4"
+
     process.selectedPatPFParticles.cut = "abs(pdgId())==13"
     process.out.outputCommands.append('keep *_selectedPatMuons_*_*')
     
+    # define filter with muon-pt above 100 GeV
+    # NB: it is selectedPatMuons that are saved in the event!
+    # NB: highPtMuons is only used for filtering when invoked
+    process.highPtMuons = process.selectedPatMuons.clone()
+    process.highPtMuons.cut = "pt > 100. & abs(eta) < 2.4"
+    
+    process.highPtMuonFilter = cms.EDFilter("CandViewCountFilter",
+                                            src = cms.InputTag("highPtMuons"),
+                                            minNumber = cms.uint32(1)
+                                            )
