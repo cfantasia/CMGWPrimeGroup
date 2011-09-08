7379d2b64f97f62d0ad4bc0f5c3839f9d62b7b75
cleonido <>
2011-09-08 22:17:49 +0000

    fix indendation

--- python/patTuple_el_cfg.py
+++ python/patTuple_el_cfg.py
@@ -2,7 +2,7 @@ from UserCode.CMGWPrimeGroup.patTuple_common_cfg import *
 
 def el_config(process) :
     # event content to include all electrons within |eta|<2.5 with pt>20
-     process.selectedPatElectrons.cut = "pt > 20. & abs(eta) < 2.5"
+    process.selectedPatElectrons.cut = "pt > 20. & abs(eta) < 2.5"
     
     process.selectedPatPFParticles.cut = "abs(pdgId())==11"
     process.out.outputCommands.append('keep *_selectedPatElectrons_*_*')
