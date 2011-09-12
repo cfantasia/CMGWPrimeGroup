46669c6ab142a047099349f5a081bee1f75b3e43
fantasia <>
2011-09-12 10:43:27 +0000

    added def Ht cut

--- bin/analyzeWprimeWZ_cfg.py
+++ bin/analyzeWprimeWZ_cfg.py
@@ -1,9 +1,11 @@
 from UserCode.CMGWPrimeGroup.commonWZ_cfg import *
 
-process.WprimeAnalyzer.outputFile  = "Wprime_analysis.root"
-process.WprimeAnalyzer.logFile     = "Wprime_event_counts.txt"
-process.WprimeAnalyzer.candEvtFile = "Wprime_CandEvts.txt"
+process.WprimeAnalyzer.outputFile  = "WprimeWZ_analysis.root"
+process.WprimeAnalyzer.logFile     = "WprimeWZ_event_counts.txt"
+process.WprimeAnalyzer.candEvtFile = "WprimeWZ_CandEvts.txt"
 
 process.WprimeAnalyzer.Cuts = WprimeWZCuts
+
+process.WprimeAnalyzer.minHt = 300. #W'600 Cut
 
 #process.WprimeAnalyzer.maxEvents   = 100
