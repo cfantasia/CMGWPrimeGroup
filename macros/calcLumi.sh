cfbe91edea389ce96c65fe419344c515d6d9af00
fantasia <>
2011-09-19 12:40:38 +0000

    Updated for Run2011B

--- macros/calcLumi.sh
+++ macros/calcLumi.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-JSON_May10=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt
-MIN_May10=160404
-MAX_May10=163869
-./jsonrunsel.py $MIN_May10   $MAX_May10   $JSON_May10   May10.json
+#Up to run 176023: 2.301 inv fb
 
-JSON_Prompt4=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-173664_7TeV_PromptReco_Collisions11_JSON.txt
-MIN_Prompt4=165088
-MAX_Prompt4=167913
-./jsonrunsel.py $MIN_Prompt4 $MAX_Prompt4 $JSON_Prompt4 Prompt4.json
+JSON_Run2011A_May10=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt
+MIN_Run2011A_May10=160404
+MAX_Run2011A_May10=163869
+./jsonrunsel.py $MIN_Run2011A_May10   $MAX_Run2011A_May10   $JSON_Run2011A_May10   Run2011A_May10.json
 
-JSON_Aug05=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt
-MIN_Aug05=170249
-MAX_Aug05=172619
-./jsonrunsel.py $MIN_Aug05   $MAX_Aug05   $JSON_Aug05   Aug05.json
+JSON_Run2011A_Prompt4=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-176023_7TeV_PromptReco_Collisions11_JSON.txt
+MIN_Run2011A_Prompt4=165088
+MAX_Run2011A_Prompt4=167913
+./jsonrunsel.py $MIN_Run2011A_Prompt4 $MAX_Run2011A_Prompt4 $JSON_Run2011A_Prompt4 Run2011A_Prompt4.json
 
-JSON_Prompt6=$JSON_Prompt4
-MIN_Prompt6=172620
-MAX_Prompt6=999999
-./jsonrunsel.py $MIN_Prompt6 $MAX_Prompt6 $JSON_Prompt6 Prompt6.json
+JSON_Run2011A_Aug05=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt
+MIN_Run2011A_Aug05=170249
+MAX_Run2011A_Aug05=172619
+./jsonrunsel.py $MIN_Run2011A_Aug05   $MAX_Run2011A_Aug05   $JSON_Run2011A_Aug05   Run2011A_Aug05.json
+
+JSON_Run2011A_Prompt6=$JSON_Run2011A_Prompt4
+MIN_Run2011A_Prompt6=172620
+MAX_Run2011A_Prompt6=173692
+./jsonrunsel.py $MIN_Run2011A_Prompt6 $MAX_Run2011A_Prompt6 $JSON_Run2011A_Prompt6 Run2011A_Prompt6.json
+
+JSON_Run2011B_Prompt1=$JSON_Run2011A_Prompt4
+MIN_Run2011B_Prompt1=175860
+MAX_Run2011B_Prompt1=999999
+./jsonrunsel.py $MIN_Run2011B_Prompt1 $MAX_Run2011B_Prompt1 $JSON_Run2011B_Prompt1 Run2011B_Prompt1.json
 
 rm -f final.json
 
-compareJSON.py --or May10.json Prompt4.json finalA.json
-compareJSON.py --or Aug05.json Prompt6.json finalB.json
-compareJSON.py --or finalA.json finalB.json final.json
+compareJSON.py --or Run2011A_May10.json Run2011A_Prompt4.json finalA.json
+compareJSON.py --or Run2011A_Aug05.json finalA.json finalB.json
+compareJSON.py --or Run2011A_Prompt6.json finalB.json finalC.json
+compareJSON.py --or Run2011B_Prompt1.json finalC.json finalD.json
 
-rm finalA.json finalB.json
+cp finalD.json final.json
+
+rm finalA.json finalB.json finalC.json finalD.json
 
 lumiCalc2.py -i final.json overview
