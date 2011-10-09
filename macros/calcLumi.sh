a9b59e578e92c55e03d18517fd104f2ea8c6048b
fantasia <>
2011-10-09 09:46:50 +0000

    Updated for latest lumi

--- macros/calcLumi.sh
+++ macros/calcLumi.sh
@@ -3,13 +3,14 @@
 #Up to run 176023: 2.301 inv fb
 #Up to run 176309: 2.510 inv fb
 #Up to run 177053: 2.915 inv fb
+#Up to run 177515: 3.193 inv fb
 
 JSON_Run2011A_May10=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt
 MIN_Run2011A_May10=160404
 MAX_Run2011A_May10=163869
 ./jsonrunsel.py $MIN_Run2011A_May10   $MAX_Run2011A_May10   $JSON_Run2011A_May10   Run2011A_May10.json
 
-JSON_Run2011A_Prompt4=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-177053_7TeV_PromptReco_Collisions11_JSON.txt
+JSON_Run2011A_Prompt4=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-177515_7TeV_PromptReco_Collisions11_JSON.txt
 MIN_Run2011A_Prompt4=165088
 MAX_Run2011A_Prompt4=167913
 ./jsonrunsel.py $MIN_Run2011A_Prompt4 $MAX_Run2011A_Prompt4 $JSON_Run2011A_Prompt4 Run2011A_Prompt4.json
