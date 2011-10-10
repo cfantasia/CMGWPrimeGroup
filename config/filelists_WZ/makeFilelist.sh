09d74af6942faf9bd20342edf6cf96ad84fa39cc
fantasia <>
2011-10-10 07:37:10 +0000

    Updated filelists

--- config/filelists_WZ/makeFilelist.sh
+++ config/filelists_WZ/makeFilelist.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-Ver="W05-08-02"
-Dir=/hdfs/store/user/jklukas
-#Dir=/pnfs/cms/WAX/11/store/user/fantasia/42X
+Ver="B05-08-10D"
+#Dir=/hdfs/store/user/jklukas
+Dir=/pnfs/cms/WAX/11/store/user/fantasia/42X
 #Dir=~/nobackup/42X/filelists
 
-for Directory in `ls ${Dir} | grep ${Ver} | grep PatTuple`
+for Directory in `ls ${Dir} | grep ${Ver} | grep Pat`
   do
   if [ -d $Dir/$Directory ]; then
       find ${Dir}/${Directory}/ | grep .root >& ${Directory}.txt
   fi
 done
 
-./mergeFilelists.sh
+#./mergeFilelists.sh
