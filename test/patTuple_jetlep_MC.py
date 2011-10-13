68554a4ecc3854142121b33ece9e1307323c77e5
fladias <>
2011-10-13 13:53:35 +0000

    keep genParticles

--- test/patTuple_jetlep_MC.py
+++ test/patTuple_jetlep_MC.py
@@ -1,3 +1,6 @@
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+
 from UserCode.CMGWPrimeGroup.patTuple_jetlep_cfg import *
 from UserCode.CMGWPrimeGroup.patTuple_mc_cfg import *
 
@@ -6,10 +9,12 @@ from UserCode.CMGWPrimeGroup.patTuple_mc_cfg import *
 jetlep_config(process, 1000, -1)
 mc_config(process, cms)
 
-process.p.replace(process.patMuons, process.muonMatch+process.patMuons)
+process.p.replace(process.patMuons, process.muonMatch+process.patMuons+process.prunedGenParticles)
 process.p.replace(process.patElectrons, process.electronMatch+process.patElectrons)
 
 process.source.fileNames = [
-    'file:/data/fladias/RSGravitonToZZToMuMuJJ_kMpl005_M_1000_pythia6_cff_py_GEN_FASTSIM_HLT.root'
+
+    'file:/home/fladias/WPrime/RSGravitonToZZToEEJJ_kMpl005_M_750_pythia6_cff_py_GEN_FASTSIM_HLT.root'
+#    'file:/data/fladias/RSGravitonToZZToMuMuJJ_kMpl005_M_1000_pythia6_cff_py_GEN_FASTSIM_HLT.root'
 #    'file:/afs/cern.ch/user/t/tomei/public/PYTHIA6_EXOTICA_RSGravZZ_kMpl005_M1000_7TeV_mumujj_cff_py_GEN_FASTSIM_HLT.root',
     ] 
