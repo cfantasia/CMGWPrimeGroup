c244a8a9fb8b42645ad295f11f36969600db67f8
fantasia <>
2011-10-28 15:29:05 +0000

    Updated for new data

--- macros/calcLumi.sh
+++ macros/calcLumi.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+#Up to run 163869: 0.215 inv fb
+#Up to run 167913: 1.141 inv fb 
+#Up to run 172619: 1.507 inv fb
+#Up to run 173692: 2.160 inv fb
 #Up to run 176023: 2.301 inv fb
 #Up to run 176309: 2.510 inv fb
 #Up to run 177053: 2.915 inv fb
 #Up to run 177515: 3.193 inv fb
 #Up to run 178078: 3.537 inv fb
+#Up to run 178677: 3.965 inv fb
 
 JSON_Run2011A_May10=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON_v3.txt
 MIN_Run2011A_May10=160404
 MAX_Run2011A_May10=163869
 ./jsonrunsel.py $MIN_Run2011A_May10   $MAX_Run2011A_May10   $JSON_Run2011A_May10   Run2011A_May10.json
 
-JSON_Run2011A_Prompt4=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-178078_7TeV_PromptReco_Collisions11_JSON.txt
+JSON_Run2011A_Prompt4=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-178677_7TeV_PromptReco_Collisions11_JSON.txt
 MIN_Run2011A_Prompt4=165088
 MAX_Run2011A_Prompt4=167913
 ./jsonrunsel.py $MIN_Run2011A_Prompt4 $MAX_Run2011A_Prompt4 $JSON_Run2011A_Prompt4 Run2011A_Prompt4.json
 
-JSON_Run2011A_Aug05=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v2.txt
+JSON_Run2011A_Aug05=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_170249-172619_7TeV_ReReco5Aug_Collisions11_JSON_v3.txt 
 MIN_Run2011A_Aug05=170249
 MAX_Run2011A_Aug05=172619
 ./jsonrunsel.py $MIN_Run2011A_Aug05   $MAX_Run2011A_Aug05   $JSON_Run2011A_Aug05   Run2011A_Aug05.json
