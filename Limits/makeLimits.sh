911ee5b35ab05c545bdf0e70c2941b5f0a1daa2d
fantasia <>
2011-11-09 11:02:19 +0000

    updated

--- Limits/makeLimits.sh
+++ Limits/makeLimits.sh
@@ -7,14 +7,13 @@ for windFracTenths in -1
   windFrac=$Ones"p"$Tenths
   echo "Using $windFrac as half window size"
 
-#  root -b -q 'ExpectedEvts.C+("../../../WprimeWZ.root",'${windFracTenths}')' > ExpectedEvts.log 
-  root -b -q 'ExpectedEvts.C+("../../../HadVZAnalyzer.root",'${windFracTenths}', "useHists")' > ExpectedEvts.log 
+  root -b -q 'ExpectedEvts.C+("../../../WprimeWZ.root","cutValues.wz.dat",'${windFracTenths}')' > ExpectedEvts.log 
+#  root -b -q 'ExpectedEvts.C+("../../../HadVZAnalyzer.root","cutValues.VZ.dat"'${windFracTenths}', "")' > ExpectedEvts.log 
 
   /afs/hep.wisc.edu/cern/.root/root_v5.30.00.Linux-slc5_amd64-gcc4.3/bin/root -b -q -n CalcLimit.C > CalcLimit.log
 
-#  root -b -q 'PlotLimit.C+("WprimeWZ")' #For W'
-#  root -b -q 'PlotLimit.C+("TCWZ")' #For TC
-  root -b -q 'PlotLimit.C+("HadVZ")' #For VZ
+  root -b -q 'PlotLimit.C+("WprimeWZ")' #For W'
+#  root -b -q 'PlotLimit.C+("HadVZ")' #For VZ
   
   Dir=windFrac${windFrac}
   if [ ! -d "$Dir" ]; then
