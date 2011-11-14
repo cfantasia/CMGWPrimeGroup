aa0aa2a66bf56784fbdebf5f2e448ae6829be418
fantasia <>
2011-11-14 14:13:45 +0000

    updated outputs

--- bin/analyzeWZFakeRate_cfg.py
+++ bin/analyzeWZFakeRate_cfg.py
@@ -6,21 +6,19 @@ process.WprimeAnalyzer.candEvtFile = "WZFakeRateElec.evt"
 process.WprimeAnalyzer.sample_cross_sections = "samples_cross_sections_WZDilepton.txt"
 
 ######Measure Fake rate of Muons######################
-#process.WprimeAnalyzer.outputFile  = 'WZFakeRateMuon.root'
-#process.WprimeAnalyzer.logFile = "WZFakeRateMuon.dat"
-#process.WprimeAnalyzer.candEvtFile = "WZFakeRateMuon.evt"
-#process.WprimeAnalyzer.triggersToUse = SingleElecTriggers
-#process.WprimeAnalyzer.Cuts = WZFakeMuonCuts
+process.WprimeAnalyzer.outputFile  = 'WZFakeRateMuon.root'
+process.WprimeAnalyzer.logFile = "WZFakeRateMuon.dat"
+process.WprimeAnalyzer.candEvtFile = "WZFakeRateMuon.evt"
+process.WprimeAnalyzer.triggersToUse = SingleElecTriggers
+process.WprimeAnalyzer.Cuts = WZFakeMuonCuts
 ######Measure Fake rate of Electrons######################
-process.WprimeAnalyzer.outputFile  = 'WZFakeRateElec.root'
-process.WprimeAnalyzer.logFile = "WZFakeRateElec.dat"
-process.WprimeAnalyzer.candEvtFile = "WZFakeRateElec.evt"
-process.WprimeAnalyzer.triggersToUse = SingleMuonTriggers
-process.WprimeAnalyzer.Cuts = WZFakeElecCuts
+#process.WprimeAnalyzer.outputFile  = 'WZFakeRateElec.root'
+#process.WprimeAnalyzer.logFile = "WZFakeRateElec.dat"
+#process.WprimeAnalyzer.candEvtFile = "WZFakeRateElec.evt"
+#process.WprimeAnalyzer.triggersToUse = SingleMuonTriggers
+#process.WprimeAnalyzer.Cuts = WZFakeElecCuts
 
 process.WprimeAnalyzer.LooseElectronType = "WZRelaxed"
 process.WprimeAnalyzer.TightElectronType = "WZTight"
 process.WprimeAnalyzer.LooseMuonType = "WZRelaxed"
 process.WprimeAnalyzer.TightMuonType = "WZTight"
-
-process.WprimeAnalyzer.minNLeptons = cms.untracked.uint32(2)
