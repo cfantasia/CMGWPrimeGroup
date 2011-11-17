6f3013791abcc03a34d7445bf4ebb59cb859f560
fantasia <>
2011-11-17 09:56:57 +0000

    helpful tool to make input filelists from dcache

---
+++ config/filelists_HadVZ/makeFilelist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Ver="V330G"
+#Dir=/hdfs/store/user/jklukas
+Dir=/pnfs/cms/WAX/11/store/user/fantasia/42X
+#Dir=~/nobackup/42X/filelists
+
+for Directory in `ls ${Dir} | grep ${Ver} | grep Run2011`
+  do
+  if [ -d $Dir/$Directory ]; then
+      find ${Dir}/${Directory}/ | grep .root >& ${Directory}.txt
+  fi
+done
+
+/usr/bin/perl -p -i -e "s/\/pnfs\/cms\/WAX\/11//g" *${Ver}*.txt
