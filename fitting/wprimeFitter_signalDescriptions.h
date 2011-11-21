c4c37c53d82f675e4f3f226dfd30bb5bb3a612dd
cleonido <>
2011-11-21 16:26:35 +0000

    rename variable

--- fitting/wprimeFitter_signalDescriptions.h
+++ fitting/wprimeFitter_signalDescriptions.h
@@ -1,30 +1,30 @@
 #ifndef _wprimeFitter_signalDescriptions_h_
 #define _wprimeFitter_signalDescriptions_h_
 
-const unsigned Nsignal = 9; // 1 bgd-only + 8 true-signal mass points
+const unsigned Nsignal_points = 9; // 1 bgd-only + 8 true-signal mass points
 
 // the first point corresponds to bgd-only distributions (ie. not a real W')
 // the first mass value is used for the S+B hypothesis fit;
 // may have to revisit this argument
-const string dirname[Nsignal] = {"wprime2.5", "wprime1.5", "wprime1.6", "wprime1.7", "wprime1.8", "wprime1.9", "wprime2.0", "wprime2.1", "wprime2.2"};
+const string dirname[Nsignal_points] = {"wprime2.5", "wprime1.5", "wprime1.6", "wprime1.7", "wprime1.8", "wprime1.9", "wprime2.0", "wprime2.1", "wprime2.2"};
 
 
-const string desc[Nsignal] = {"SM", "W' (1.5 TeV)", "W' (1.6 TeV)", 
+const string desc[Nsignal_points] = {"SM", "W' (1.5 TeV)", "W' (1.6 TeV)", 
 			      "W' (1.7 TeV)", "W' (1.8 TeV)", "W' (1.9 TeV)",
 			      "W' (2.0 TeV)", "W' (2.1 TeV)", "W' (2.2 TeV)"};
 
-const float WprimeMass[Nsignal] = {2500, 1500, 1600, 1700, 1800, 1900, 2000, 
+const float WprimeMass[Nsignal_points] = {2500, 1500, 1600, 1700, 1800, 1900, 2000, 
 				   2100, 2200};
 
 // SSM cross-sections for leptonic channels on pb;
 // these will be divided by scale factor
 // first point corresponds to bgd-only case: cross-section = 0
-const float xsec_lep[Nsignal] = {
+const float xsec_lep[Nsignal_points] = {
   0, 0.0949, 0.0633, 0.0424, 0.0285, 0.0194, 0.0135, 0.009373, 0.006605};
 
 
 // color for plotting Z-values of different signals
-Color_t color[Nsignal] = {kBlack, kRed, kBlue, kViolet, kGreen, kOrange, kYellow, kViolet, kOrange};
+Color_t color[Nsignal_points] = {kBlack, kRed, kBlue, kViolet, kGreen, kOrange, kYellow, kViolet, kOrange};
 
 
 // this structure is used to keep track of expected bgd, sig 
@@ -36,7 +36,7 @@ struct Nexp{
   float Nsig;
 };
 
-//Nexp Nevt[Nsignal];
+//Nexp Nevt[Nsignal_points];
 
 const float M_W = 80.399;
 const float G_W = 2.085;
