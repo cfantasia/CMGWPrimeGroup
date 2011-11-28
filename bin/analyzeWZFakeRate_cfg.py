bdc5025fe255b077257df1ab351e9616aedaeec5
fantasia <>
2011-11-28 10:27:53 +0000

    updated fake rate cuts

--- bin/analyzeWZFakeRate_cfg.py
+++ bin/analyzeWZFakeRate_cfg.py
@@ -1,24 +1,33 @@
 from UserCode.CMGWPrimeGroup.commonWZ_cfg import *
 
-process.WprimeAnalyzer.outputFile  = 'WZFakeRateElec.root'
-process.WprimeAnalyzer.logFile = "WZFakeRateElec.dat"
-process.WprimeAnalyzer.candEvtFile = "WZFakeRateElec.evt"
 process.WprimeAnalyzer.sample_cross_sections = "samples_cross_sections_WZDilepton.txt"
 
 ######Measure Fake rate of Muons######################
-process.WprimeAnalyzer.outputFile  = 'WZFakeRateMuon.root'
-process.WprimeAnalyzer.logFile = "WZFakeRateMuon.dat"
-process.WprimeAnalyzer.candEvtFile = "WZFakeRateMuon.evt"
-process.WprimeAnalyzer.triggersToUse = SingleElecTriggers
-process.WprimeAnalyzer.Cuts = WZFakeMuonCuts
-######Measure Fake rate of Electrons######################
-#process.WprimeAnalyzer.outputFile  = 'WZFakeRateElec.root'
-#process.WprimeAnalyzer.logFile = "WZFakeRateElec.dat"
-#process.WprimeAnalyzer.candEvtFile = "WZFakeRateElec.evt"
-#process.WprimeAnalyzer.triggersToUse = SingleMuonTriggers
-#process.WprimeAnalyzer.Cuts = WZFakeElecCuts
+if False:
+#if True:
+    print 'Determining Fake Rate of Electrons'
+    process.WprimeAnalyzer.outputFile  = 'WZFakeRateElec.root'
+    process.WprimeAnalyzer.logFile = "WZFakeRateElec.dat"
+    process.WprimeAnalyzer.candEvtFile = "WZFakeRateElec.evt"
+    process.WprimeAnalyzer.triggersToUse = SingleMuonTriggers
+    process.WprimeAnalyzer.Cuts = WZFakeElecCuts
+else :
+    print 'Determining Fake Rate of Muons'
+    process.WprimeAnalyzer.outputFile  = 'WZFakeRateMuon.root'
+    process.WprimeAnalyzer.logFile = "WZFakeRateMuon.dat"
+    process.WprimeAnalyzer.candEvtFile = "WZFakeRateMuon.evt"
+    process.WprimeAnalyzer.triggersToUse = SingleElecTriggers
+    process.WprimeAnalyzer.Cuts = WZFakeMuonCuts
 
 process.WprimeAnalyzer.LooseElectronType = "WZRelaxed"
 process.WprimeAnalyzer.TightElectronType = "WZTight"
 process.WprimeAnalyzer.LooseMuonType = "WZRelaxed"
 process.WprimeAnalyzer.TightMuonType = "WZTight"
+
+###Cuts
+process.WprimeAnalyzer.minWtransMass = 30.
+process.WprimeAnalyzer.minMET = 30.
+process.WprimeAnalyzer.minMET = 30.
+process.WprimeAnalyzer.minNLeptons = 2
+process.WprimeAnalyzer.minNTightLeptons = cms.untracked.uint32(1)
+
