93f5248139a8e701c22201f938ccd1e7c533d091
cleonido <>
2011-11-30 11:01:50 +0000

    only median point for now, to be able to run quicker tests

--- fitting/fit_wprime.C
+++ fitting/fit_wprime.C
@@ -13,7 +13,7 @@ void fit_wprime()
   a.setNpseudoExperiments(1000);
   a.doOneMassPointOnly(false);
   a.doRunFits(true);
-  a.findOnlyMedian(false);
+  a.findOnlyMedian(true);
   
   /* method WprimeFitter::run() loops over all mass points listed in 
      wprimeFitter_signalDescription.h; for each mass point it 
