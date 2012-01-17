5e0e45a3840a0e743506f3413d6f49af6e3ee416
fladias <>
2012-01-17 19:19:23 +0000

    make kt6pfrho

--- python/patTuple_jetlep_cfg.py
+++ python/patTuple_jetlep_cfg.py
@@ -4,6 +4,15 @@ from UserCode.CMGWPrimeGroup.patTuple_mu_cfg import *
 from UserCode.CMGWPrimeGroup.patTuple_jet_cfg import *
 from UserCode.CMGWPrimeGroup.patTuple_met_cfg import *
 
+
+def addFastJet(process):
+    process.load('RecoJets.JetProducers.kt4PFJets_cfi')
+    process.kt6PFJets = process.kt4PFJets.clone(rParam=0.6, doRhoFastjet=True)
+    process.kt6PFJets.Rho_EtaMax = cms.double(2.5)
+#    process.patDefaultSequence.replace(process.patJetCorrFactors,
+#                                       process.kt6PFJets *
+#                                       process.patJetCorrFactors)
+    
 def jetlep_config(process, reportEveryNum=100, maxEvents=-1) :
     process.load("UserCode.CMGWPrimeGroup.patTuple_jet_cfg")
     common_config(process, reportEveryNum, maxEvents)
@@ -11,6 +20,7 @@ def jetlep_config(process, reportEveryNum=100, maxEvents=-1) :
     el_config(process)
     mu_config(process)
     met_config(process)
+    addFastJet(process)
 
     # redefine selectedPatMuons (isGlobalMuon not included in std definition)
     process.selectedPatMuons.cut = "pt > 20. & abs(eta) < 2.4 & isGlobalMuon"
@@ -33,7 +43,8 @@ def jetlep_config(process, reportEveryNum=100, maxEvents=-1) :
         process.goodOfflinePrimaryVertices*
         getattr(process,"PF2PATmod")* 
         #process.PF2PATmod *
-        (process.patJetCorrFactors +
+        ((process.kt6PFJets*
+         process.patJetCorrFactors) +
          process.patJets +
          process.selectedPatJets +
          process.countPatJets
@@ -54,17 +65,22 @@ def jetlep_config(process, reportEveryNum=100, maxEvents=-1) :
 #    from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
 #    process.kt6PFJetsPFlow = kt4PFJets.clone(
 #        rParam = cms.double(0.6),
-#        src = cms.InputTag('pfNoElectron'),
+#        src = cms.InputTag('patDefaultSequence'),
 #        doAreaFastjet = cms.bool(True),
 #        doRhoFastjet = cms.bool(True)
 #        )
+
+
     process.patJetCorrFactors.rho = cms.InputTag("kt6PFJetsPFlow", "rho")
-    process.out.outputCommands.append('keep *_kt6PFJetsPFlow_*_PAT')
+    process.out.outputCommands.append('keep *_kt6PFJets_*_PAT')
 
 #    process.out.outputCommands.append('keep *')
-    
+#    
 #    getattr(process,"PF2PATmod").replace(
 #        getattr(process,"patDefaultSequence"),
 #        getattr(process,"patDefaultSequence")*process.kt6PFJetsPFlow )
+
+#    process.SkipEvent = cms.untracked.vstring('ProductNotFound')
+
   
-#    print process.patDefaultSequence
+#print process.patDefaultSequence
