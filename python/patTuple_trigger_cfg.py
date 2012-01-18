991dc8df0729cfd75a3f7ba4468d7c9470a5b495
fantasia <>
2012-01-18 14:43:45 +0000

    updated trigger list

--- python/patTuple_trigger_cfg.py
+++ python/patTuple_trigger_cfg.py
@@ -1,41 +1,29 @@
 from PhysicsTools.PatAlgos.tools.coreTools import *
 
 singleMuonPaths = [
-    'HLT_Mu11',          # from 2010 data
     'HLT_Mu15_v*',
     'HLT_Mu17_v*',
     'HLT_Mu20_v*',
     'HLT_Mu24_v*',
     'HLT_Mu30_v*',
-    'HLT_IsoMu15_v*',
-    'HLT_IsoMu17_v*',
-    'HLT_IsoMu24_v*',
-    'HLT_IsoMu30_v*',
-    'HLT_IsoMu40_v*',
+    'HLT_Mu40_v*',
     'HLT_Mu40_eta2p1_v*',
     'HLT_Mu50_eta2p1_v*',
     ]
 
 doubleMuonPaths = [
-    'HLT_DoubleMu5_v*',
     'HLT_DoubleMu7_v*',
-    'HLT_TripleMu5_v*',
     'HLT_Mu13_Mu8_v*', #1e33 unprescaled
     'HLT_Mu17_Mu8_v*' #3e33 unprescaled
-    'HLT_Mu17_TkMu8_v*',
+    'HLT_Mu17_TkMu8_v*', #2011B
     ]
 
 singleElectronPaths = [
-    'HLT_Ele17_SW_L1R',  # from 2010 data
-    'HLT_Ele17_SW_Isol_L1R_v*',
-    'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*',
-    'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*',
-    'HLT_Ele42_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*' #2e33(?) unprescaled
     ]
 
 doubleElectronPaths = [
-    'HLT_DoubleEle17_SW_L1R_v*',
     'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v*',
+    'HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v*',#early version, MC
     'HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*'
     ]
 
