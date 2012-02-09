ede5e166fdca01e53e20070e7821a6275c22d669
fantasia <>
2012-02-09 08:42:53 +0000

    new filelists for single mu and fall11 dy

--- config/filelists_HadVZ/makeFilelist.sh
+++ config/filelists_HadVZ/makeFilelist.sh
@@ -4,7 +4,7 @@ Ver="V360B"
 Dir=/pnfs/cms/WAX/11/store/user/fantasia/42X
 #Dir=~/nobackup/42X/filelists
 
-for Directory in `ls ${Dir} | grep ${Ver} | grep Run2011`
+for Directory in `ls ${Dir} | grep ${Ver}` #| grep Run2011`
   do
   if [ -d $Dir/$Directory ]; then
       find ${Dir}/${Directory}/ | grep .root >& ${Directory}.txt
