cefe87fdfbf4ba57dea3da74a6605de22e92b7d1
fladias <>
2012-02-16 20:23:23 +0000

    Fiz jets CHS

--- test/patTuple_jetlep_MC.py
+++ test/patTuple_jetlep_MC.py
@@ -12,9 +12,14 @@ mc_config(process, cms)
 process.p.replace(process.patMuons, process.muonMatch+process.patMuons+process.prunedGenParticles)
 process.p.replace(process.patElectrons, process.electronMatch+process.patElectrons)
 
-process.source.fileNames = [
+process.p.replace(process.pfNoPileUpIso, process.pfNoPileUpIso*process.newAK7PF)
 
-    'file:/home/fladias/WPrime/RSGravitonToZZToEEJJ_kMpl005_M_750_pythia6_cff_py_GEN_FASTSIM_HLT.root'
+process.patJetsAK7PF.jetSource = cms.InputTag("newAK7PF")
+
+
+process.source.fileNames = [
+    '/store/mc/Fall11/WprimeToWZTo2Q2L_M-1000_7TeV-pythia6-tauola/AODSIM/PU_S6_START42_V14B-v1/0000/B8733CA2-DA28-E111-B278-00215E21D702.root'
+#    'file:/home/fladias/WPrime/RSGravitonToZZToEEJJ_kMpl005_M_750_pythia6_cff_py_GEN_FASTSIM_HLT.root'
 #    'file:/data/fladias/RSGravitonToZZToMuMuJJ_kMpl005_M_1000_pythia6_cff_py_GEN_FASTSIM_HLT.root'
 #    'file:/afs/cern.ch/user/t/tomei/public/PYTHIA6_EXOTICA_RSGravZZ_kMpl005_M1000_7TeV_mumujj_cff_py_GEN_FASTSIM_HLT.root',
     ] 
