731d5637a41169b2d71a5dae7a345510c0874c83
fantasia <>
2012-03-28 06:38:25 +0000

    updated for new dir structure

--- config/filelists_WZ/makeFilelist.sh
+++ config/filelists_WZ/makeFilelist.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-Ver="B05-08-10H"
-#Dir=/hdfs/store/user/jklukas
-Dir=/pnfs/cms/WAX/11/store/user/fantasia/42X
-#Dir=~/nobackup/42X/filelists
+Ver="W06-06-05A"
+#Base=/hdfs/store/user/jklukas
+Base=/pnfs/cms/WAX/11/store/user/fantasia/42X
+#Base=~/nobackup/42X/filelists
 
-for Directory in `ls ${Dir} | grep ${Ver} | grep Pat`
+for Directory in `ls ${Base}/ | grep ${Ver} | grep ${Ver}`
   do
-  if [ -d $Dir/$Directory ]; then
-      find ${Dir}/${Directory}/ | grep .root >& ${Directory}.txt
-  fi
+  #echo Directory is $Directory
+  for Sample in `ls ${Base}/${Directory}/`
+    do
+    #echo Sample is $Sample
+    for List in `ls ${Base}/${Directory}/${Sample}/`
+      do
+      #echo List is $List
+      if [ -d $Base/$Directory/$Sample/$List ]; then
+          find ${Base}/${Directory}/$Sample/$List | grep .root >& ${Ver}-${List}.txt
+      fi
+    done
+  done
 done
 
-/usr/bin/perl -p -i -e "s/\/pnfs\/cms\/WAX\/11//g" *${Ver}*.txt
+#Strip off prefix
+/usr/bin/perl -p -i -e "s/\/pnfs\/cms\/WAX\/11//g" *.txt
 #/usr/bin/perl -p -i -e "s/\/hdfs//g" *.txt
 #./mergeFilelists.sh
