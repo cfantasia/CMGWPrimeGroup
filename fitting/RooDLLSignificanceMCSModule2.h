a3e04f085c25b80fbf9cbb19a0b976c23ab1ff61
cleonido <>
2012-04-19 15:33:03 +0000

    add NLL(H1), remove chi2 & friends

--- fitting/RooDLLSignificanceMCSModule2.h
+++ fitting/RooDLLSignificanceMCSModule2.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Project: RooFit                                                           *
  * Package: RooFitCore                                                       *
- *    File: $Id: RooDLLSignificanceMCSModule.h,v 1.2 2007/05/11 09:11:30 verkerke Exp $
+ *    File: $Id: RooDLLSignificanceMCSModule2.h,v 1.1 2012/04/16 14:41:08 cleonido Exp $
  * Authors:                                                                  *
  *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
  *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
@@ -45,9 +45,12 @@ class RooDLLSignificanceMCSModule2 : public RooAbsMCStudyModule {
   std::string _parName ;  // Name of Nsignal parameter
   RooDataSet* _data ;     // Summary dataset to store results
   RooRealVar* _nll0h ;    // Container variable for NLL result on null hypothesis
-  RooRealVar* _dll0h ;    // Container variable for delta NLL 
+  RooRealVar* _nll1h ;    // Container variable for NLL result on H1 hypothesis
+  RooRealVar* _dllh ;    // Container variable for delta NLL 
   RooRealVar* _sig0h ;    // Container variable for NLL result with signal
   Double_t    _nullValue ;  // Numeric value of Nsignal parameter representing the null hypothesis
+
+  /*
   RooRealVar* _chi2H0 ;    // Chi^2 of function w.r.t. data for H0
   RooRealVar* _ndofH0 ;    // Number of degrees of freedom for H0
   RooRealVar* _chi2redH0 ; // Reduced Chi^2 w.r.t data for H0
@@ -56,6 +59,7 @@ class RooDLLSignificanceMCSModule2 : public RooAbsMCStudyModule {
   RooRealVar* _ndofH1 ;    // Number of degrees of freedom for H1
   RooRealVar* _chi2redH1 ; // Reduced Chi^2 w.r.t data for H1
   RooRealVar* _probH1 ;    // Probability of chi^2,nDOF combination for H1
+  */
   RooFitResult* frnull ;      // Fit results for H0
 
   ClassDef(RooDLLSignificanceMCSModule2,1) // MCStudy module to calculate Delta(-logL) significance w.r.t given null hypothesis
