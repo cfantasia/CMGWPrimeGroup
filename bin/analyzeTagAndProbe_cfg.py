a6425883ea568d1bb36360474a223a38e0305e7e
fantasia <>
2013-03-20 17:21:50 +0000

    added id,iso selections

--- bin/analyzeTagAndProbe_cfg.py
+++ bin/analyzeTagAndProbe_cfg.py
@@ -14,15 +14,37 @@ process.WprimeAnalyzer.sample_cross_sections = "samples_cross_sections_WZDilepto
 
 process.WprimeAnalyzer.Cuts = WZEffCuts
 
+#Object Def
 process.WprimeAnalyzer.CountElectronType = cms.untracked.string("CiC2012Loose")
 process.WprimeAnalyzer.TagElectronType   = cms.untracked.string("CiC2012Medium")
-process.WprimeAnalyzer.LooseProbeElectronType = cms.untracked.string("CiC2012LooseRelaxed")
-process.WprimeAnalyzer.TightProbeElectronType = cms.untracked.string("CiC2012Loose")
+process.WprimeAnalyzer.CountMuonType = cms.untracked.string("PFIsoHighPtBoostedZLoose")
+process.WprimeAnalyzer.TagMuonType   = cms.untracked.string("PFIsoHighPtBoostedZTight")
 
-process.WprimeAnalyzer.CountMuonType = cms.untracked.string("EWKWZLoose")
-process.WprimeAnalyzer.TagMuonType   = cms.untracked.string("EWKWZTight")
-process.WprimeAnalyzer.LooseProbeMuonType = cms.untracked.string("EWKWZRelaxed")
-process.WprimeAnalyzer.TightProbeMuonType = cms.untracked.string("EWKWZLoose")
+### Choose 1 block #########
+# Z Lepton ID:
+process.WprimeAnalyzer.LooseProbeElectronType = cms.untracked.string("Reco")
+process.WprimeAnalyzer.TightProbeElectronType = cms.untracked.string("CiC2012LooseRelaxed")
+process.WprimeAnalyzer.LooseProbeMuonType = cms.untracked.string("Reco")
+process.WprimeAnalyzer.TightProbeMuonType = cms.untracked.string("PFIsoHighPtBoostedZRelaxed")
+
+#Z Lepton Iso:
+#process.WprimeAnalyzer.LooseProbeElectronType = cms.untracked.string("CiC2012LooseRelaxed")
+#process.WprimeAnalyzer.TightProbeElectronType = cms.untracked.string("CiC2012Loose")
+#process.WprimeAnalyzer.LooseProbeMuonType = cms.untracked.string("PFIsoHighPtBoostedZRelaxed")
+#process.WprimeAnalyzer.TightProbeMuonType = cms.untracked.string("PFIsoHighPtBoostedZTight")
+
+#W Lepton ID:
+#process.WprimeAnalyzer.LooseProbeElectronType = cms.untracked.string("Reco")
+#process.WprimeAnalyzer.TightProbeElectronType = cms.untracked.string("CiC2012MediumRelaxed")
+#process.WprimeAnalyzer.LooseProbeMuonType = cms.untracked.string("Reco")
+#process.WprimeAnalyzer.TightProbeMuonType = cms.untracked.string("PFIsoHighPtRelaxed")
+
+#W Lepton Iso:
+#process.WprimeAnalyzer.LooseProbeElectronType = cms.untracked.string("CiC2012MediumRelaxed")
+#process.WprimeAnalyzer.TightProbeElectronType = cms.untracked.string("CiC2012Medium")
+#process.WprimeAnalyzer.LooseProbeMuonType = cms.untracked.string("PFIsoHighPtRelaxed")
+#process.WprimeAnalyzer.TightProbeMuonType = cms.untracked.string("PFIsoHighPtTight")
+####End Choices
 
 # +++++++++++++++++++General Cut values
 process.WprimeAnalyzer.maxNumZs = cms.uint32(1)
@@ -30,7 +52,7 @@ process.WprimeAnalyzer.minNLeptons = 1
 
 # +++++++++++++++++++Z Cuts
 process.WprimeAnalyzer.minZeePt1 =  cms.double(35.)
-process.WprimeAnalyzer.minZeePt2 =  cms.double(35.)
+process.WprimeAnalyzer.minZeePt2 =  cms.double(20.)
 process.WprimeAnalyzer.minZmmPt1 =  cms.double(25.)
 process.WprimeAnalyzer.minZmmPt2 =  cms.double(10.)
 
