a73f261d1d53260ba7f7a01c1d1c193f8d75634d
fantasia <>
2013-03-20 16:02:20 +0000

    new tables

--- root_macros/printTables.sh
+++ root_macros/printTables.sh
@@ -17,15 +17,9 @@ then
     mv -f $Output{,.bck} 
 fi
 
-#######
-cd ../Systematics
-root -b -l -q calcRecoilSys.C+ >> $Output
-root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZMuPtRes.root",   "SysMuPtRes.dat")' 
-root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZMuPtScale.root", "SysMuPtScale.dat")' 
-root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZElEnScale.root", "SysElEnScale.dat")' 
-root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZPUSys.root",     "SysPU.dat")' 
-
-#######
+####################
+###  Yields ########
+####################
 
 cd ../root_macros
 echo "% 4 Channels Combined Yields">> $Output
@@ -33,28 +27,33 @@ root -b -l -q 'printNEvts.C+('\"$Input\"', -1)' >> $Output
 echo "% 2 Channels Combined Yields">> $Output
 root -b -l -q 'printNEvts.C+('\"$Input\"', -2)' >> $Output
 echo "% Indiv Yields">> $Output
-root -b -l -q 'printNEvts.C+('\"$Input\"', -999)' >> $Output
-#root -b -l -q 'printNEvts.C+('\"$Input\"', 0)' >> $Output
-#root -b -l -q 'printNEvts.C+('\"$Input\"', 1)' >> $Output
-#root -b -l -q 'printNEvts.C+('\"$Input\"', 2)' >> $Output
-#root -b -l -q 'printNEvts.C+('\"$Input\"', 3)' >> $Output
+root -b -l -q 'printNEvts.C+('\"$Input\"', -4)' >> $Output
+root -b -l -q 'printNEvts.C+('\"$Input\"', -5)' >> $Output
 
-cd ../Limits
+####################
+###  Limits ########
+####################
 
-# #Breakdown of Bkg After Lt Cut
-# #Don't scale MC for this table
-echo "% Bkg Breakdown After Lt Cut">> $Output
- root -b -l -q 'ExpectedEvts.C+('\"$Input\"',"cutValues.wz.dat",-1, "tbl noWind")' >> $Output
- root -b -l -q 'printTable.C+(1)' >> $Output
+cd ../combined_limits
+echo "% Final Yield, Limit Tables" >> $Output
+root -b -l -q 'printTables.C+' >> $Output
 
+####################
+###  Calc Sys ######
+####################
 
-echo "% Breakdown of Bkg After LT and Window Cut">> $Output
-root -b -l -q 'ExpectedEvts.C+('\"$Input\"',"cutValues.wz.dat",-1, "tbl")' >> $Output
-root -b -l -q 'ExpectedEvts.C+('\"$Input\"',"cutValues.wz.dat",-1, "scaleMC tbl")' > /dev/null
-root -b -l -q 'printTable.C+(0)' >> $Output
+#cd ../Systematics
+#echo "% Sys Tables" >> $Output
+#root -b -l -q calcRecoilSys.C+ >> $Output
+#root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZMuPtRes.root",   "SysMuPtRes.dat")' 
+#root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZMuPtScale.root", "SysMuPtScale.dat")' 
+#root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZElEnScale.root", "SysElEnScale.dat")' 
+#root -b -l -q 'compareYields.C+('\"$Input\"', "../../../WprimeWZPUSys.root",     "SysPU.dat")' 
 
+####################
+###  Print Sys #####
+####################
 
-##############################
 cd ../root_macros
 echo "% Systematic Tables" >> $Output
 root -b -l -q 'printSysTable.C+("../Systematics/SysSigMETRes.dat",    "../Systematics/SysBkgMETRes.dat",    "../Systematics/SysMETRes.pdf")'    >> $Output
@@ -64,3 +63,18 @@ root -b -l -q 'printSysTable.C+("../Systematics/SysSigMuPtScale.dat", "../System
 root -b -l -q 'printSysTable.C+("../Systematics/SysSigElEnScale.dat", "../Systematics/SysBkgElEnScale.dat", "../Systematics/SysElEnScale.pdf")' >> $Output
 root -b -l -q 'printSysTable.C+("../Systematics/SysSigPDF.dat",       "../Systematics/SysBkgPDF.dat",       "../Systematics/SysPDF.pdf")'       >> $Output
 root -b -l -q 'printSysTable.C+("../Systematics/SysSigPU.dat",        "../Systematics/SysBkgPU.dat",        "../Systematics/SysPU.pdf")'        >> $Output
+
+#####################
+#### Plots ##########
+#####################
+
+cd ../root_macros
+echo "% Plots and Eff Tables" >> $Output
+root -b -l -q 'MakePlots.cc+('\"$Input\"', "", "")'
+
+root -b -l -q 'EffVsMass.C+('\"$Input\"', 0)' >> $Output #EWK Cuts
+root -b -l -q 'EffVsMass.C+('\"$Input\"', 1)' >> $Output #Lt  Cuts
+root -b -l -q 'EffVsMass.C+('\"$Input\"', 2)' >> $Output #Lt + Wind Cuts
+
+root -b -l -q 'PlotResolution.C+('\"$Input\"', 2000)'
+root -b -l -q 'ZptVsWpt.C+("../../../WprimeWZ.root", "")'
