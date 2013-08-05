507c19a9a1fb20ccbe6dfe6151888badef6e939f
fantasia <>
2013-08-05 18:59:14 +0000

    final 2012 limits

--- combined_limits/submit.sh
+++ combined_limits/submit.sh
@@ -1,20 +1,44 @@
 #!/bin/bash -f
 
-#for Mass in `seq 2000 -100 200`; do
-#    #./doCombinedLimits.py -M $Mass --ntoys=100 
-#    ./doCombinedLimits.py -M $Mass --ntoys=1000 --condor
-#    #./doCombinedLimits.py -M $Mass --observed
-#done
+#FullRange="`seq 150 10 190`"
+FullRange="`seq 170 10 250` `seq 275 25 350` `seq 400 50 600` `seq 700 100 2000`"
+#FullRange="`seq 400 50 600` `seq 700 100 2000`"
+#FullRange="`seq 200 100 2000`"
+Range="`seq 200 100 2000`"
 
-#for Mass in `seq 200 100 700`; do
-#    ./doCombinedLimits.py -M $Mass --observed
-#done
+echo $Range
 
-for Mass in 1000; do
-   for LtShift in `seq -100 50 100`; do
-       for WindShift in `seq -100 50 100`; do
-           #./doCombinedLimits.py -M $Mass --LtShift=$LtShift --WindShift=$WindShift
-           ./doCombinedLimits.py --LtShift=$LtShift --WindShift=$WindShift -p
-       done
-   done
-done    
+for Mass in $FullRange; do
+    echo "Mass $Mass"
+    ./doCombinedLimits.py -M $Mass --doSepCh --onlyMakeCards
+done
+
+for Mass in $FullRange; do
+    echo "Mass $Mass"
+    #./doCombinedLimits.py -M $Mass --ntoys=100 
+    ./doCombinedLimits.py -M $Mass --ntoys=1000 --condor --njobs=10 --doSepCh --doDuplicates
+    #./doCombinedLimits.py -M $Mass --observed
+done
+
+for Mass in $FullRange; do
+    echo "Mass $Mass"
+    #./doCombinedLimits.py -M $Mass --observed 
+    #./doCombinedLimits.py -M $Mass --observed --doDuplicates
+    ./doCombinedLimits.py -M $Mass --observed --doSepCh 
+    #./doCombinedLimits.py -M $Mass --observed --doSepCh --doDuplicates
+done
+
+for Mass in $FullRange; do
+    echo "Mass $Mass"
+    #./doCombinedLimits.py -M $Mass --observed --doSepCh
+done
+
+
+#for Mass in 1000; do
+#   for LtShift in `seq -100 50 100`; do
+#       for WindShift in `seq -100 50 100`; do
+#           #./doCombinedLimits.py -M $Mass --LtShift=$LtShift --WindShift=$WindShift
+#           ./doCombinedLimits.py --LtShift=$LtShift --WindShift=$WindShift -p
+#       done
+#   done
+#done    
